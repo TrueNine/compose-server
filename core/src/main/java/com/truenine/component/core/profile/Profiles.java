c17e8f5e31e1a73980958cbdc677892c56a3bc4c
T_teng <616057370@qq.com>
2023-03-25 19:16:50 +0800

    feat 常用环境类

---
+++ core/src/main/java/com/truenine/component/core/profile/Profiles.java
@@ -0,0 +1,14 @@
+package com.truenine.component.core.profile;
+
+/**
+ * 常用环境
+ *
+ * @author t_teng
+ * @since 2023-03-25
+ */
+public class Profiles {
+  public static final String DEV = "dev";
+  public static final String PRODUCT = "prod";
+  public static final String TEST = "test";
+  public static final String SNAPSHOT = "snapshot";
+}
