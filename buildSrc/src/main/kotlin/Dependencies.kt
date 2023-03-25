c388254690d0f38e673083c176c77d26377d9273
T_teng <616057370@qq.com>
2023-03-25 18:46:59 +0800

    del 暂时弃用 xxl-job

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,7 +3,7 @@
  */
 object ProjectManager {
   const val group = "com.truenine.component"
-  const val version = "0.2.49-SNAPSHOT"
+  const val version = "0.2.50-SNAPSHOT"
   const val encoding = "UTF-8"
 }
 
