a579dacec743e3a6796ae96678d172c7a2c74efc
T_teng <616057370@qq.com>
2023-04-26 21:01:11 +0800

    fix 更新 BaseController.kt

--- rds/src/main/kotlin/com/truenine/component/rds/base/BaseController.kt
+++ rds/src/main/kotlin/com/truenine/component/rds/base/BaseController.kt
@@ -12,22 +12,23 @@ abstract class BaseController<T : BaseEntity>(
   protected val service: BaseService<T>
 ) {
   @ResponseBody
-  @GetMapping("items/all")
+  @GetMapping("all")
   @Operation(summary = "分页查询所有数据")
   fun findAll(@Valid page: PagedRequestParam?): PagedResponseResult<T> = service.findAllByNotLogicDeleted(page ?: PagedWrapper.DEFAULT_MAX)
 
   @ResponseBody
-  @GetMapping("items/{id}")
-  @Operation(summary = "根据id查询单条数据")
+  @GetMapping("byId/{id}")
+  @Operation(summary = "根据id查询数据")
   fun findById(@PathVariable id: Long): T? = service.findById(id)
 
   @ResponseBody
-  @GetMapping("items/countAll")
-  @Operation(summary = "当前数据总数")
+  @GetMapping("count/all")
+  @Operation(summary = "当前所有数据总数")
   fun countAll(): Long = service.countAllByNotLogicDeleted()
 
   @ResponseBody
-  @GetMapping("items/{id}/exists")
-  @Operation(summary = "根据id查询单条数据是否存在")
+  @GetMapping("existsById/{id}")
+  @Operation(summary = "根据id查询某条数据是否存在")
   fun existsById(@PathVariable id: Long): Boolean = service.existsById(id)
+
 }
