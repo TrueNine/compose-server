4650d9420b39bf6f12f82e8c4af5900d1ae9f776
T_teng <616057370@qq.com>
2023-04-26 23:45:47 +0800

    fix 更新接口说明

--- rds/src/main/kotlin/com/truenine/component/rds/base/BaseController.kt
+++ rds/src/main/kotlin/com/truenine/component/rds/base/BaseController.kt
@@ -11,28 +11,28 @@ abstract class BaseController<T : BaseEntity>(
 ) {
   @ResponseBody
   @GetMapping("meta/all")
-  @Operation(summary = "【META】分页查询所有数据")
+  @Operation(summary = "【ǃ RDS】分页查询所有数据")
   fun findAllMeta(@Valid page: PagedRequestParam?): PagedResponseResult<T> = service.findAllByNotLogicDeleted(page ?: PagedWrapper.DEFAULT_MAX)
 
   @ResponseBody
   @GetMapping("meta/byId")
-  @Operation(summary = "【META】根据id查询数据")
+  @Operation(summary = "【ǃ RDS】根据id查询数据")
   fun findMetaById(id: Long): T? = service.findById(id)
 
   @ResponseBody
   @GetMapping("meta/count/all")
-  @Operation(summary = "【META】当前所有数据总数")
+  @Operation(summary = "【ǃ RDS】当前所有数据总数")
   fun countAllMeta(): Long = service.countAllByNotLogicDeleted()
 
   @ResponseBody
   @GetMapping("meta/exists/byId")
-  @Operation(summary = "【META】根据id查询某条数据是否存在")
+  @Operation(summary = "【ǃ RDS】根据id查询某条数据是否存在")
   fun existsMetaById(id: Long): Boolean = service.existsById(id)
 
   @ResponseBody
   @PostMapping("meta")
   @Operation(
-    summary = "【META】保存数据", description = """
+    summary = "【ǃ RDS】保存数据", description = """
     调用该接口，传入的实体id会被清除然后保存
   """
   )
@@ -41,7 +41,7 @@ abstract class BaseController<T : BaseEntity>(
   @ResponseBody
   @PostMapping("meta/all")
   @Operation(
-    summary = "【META】保存一组数据", description = """
+    summary = "【ǃ RDS】保存一组数据", description = """
     调用该接口，传入的每个实体id都会被清除
   """
   )
@@ -50,7 +50,7 @@ abstract class BaseController<T : BaseEntity>(
   @ResponseBody
   @PutMapping("meta/byId")
   @Operation(
-    summary = "【META】根据实体id修改数据", description = """
+    summary = "【ǃ RDS】根据实体id修改数据", description = """
     调用该接口，传入的实体必须存在id
   """
   )
@@ -59,7 +59,7 @@ abstract class BaseController<T : BaseEntity>(
   @ResponseBody
   @PutMapping("meta/byId/all")
   @Operation(
-    summary = "【META】根据实体id修改全部数据", description = """
+    summary = "【ǃ RDS】根据实体id修改全部数据", description = """
     调用该接口，传入的每个实体必须存在id
   """
   )
@@ -67,21 +67,21 @@ abstract class BaseController<T : BaseEntity>(
 
   @ResponseBody
   @DeleteMapping("meta/byId")
-  @Operation(summary = "【META】根据id删除数据")
+  @Operation(summary = "【ǃ RDS】根据id删除数据")
   fun deleteMetaById(id: Long) = Unit
 
   @ResponseBody
   @DeleteMapping("meta/logic/byId")
-  @Operation(summary = "【META】根据id逻辑删除数据")
+  @Operation(summary = "【ǃ RDS】根据id逻辑删除数据")
   fun logicDeleteMetaById(id: Long) = service.logicDeleteById(id)
 
   @ResponseBody
   @DeleteMapping("meta/logic/byId/all")
-  @Operation(summary = "【META】根据id逻辑删除全部数据")
+  @Operation(summary = "【ǃ RDS】根据id逻辑删除全部数据")
   fun logicDeleteAllMetaById(ids: List<Long>) = service.logicDeleteAllById(ids)
 
   @ResponseBody
   @DeleteMapping("meta/byId/all")
-  @Operation(summary = "【META】根据一组id进行删除")
+  @Operation(summary = "【ǃ RDS】根据一组id进行删除")
   fun deleteAllMetaByIds(ids: List<Long>) = Unit
 }
