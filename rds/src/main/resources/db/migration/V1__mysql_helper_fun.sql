d554d8c2f197efacb05fb102c992b91183b01e10
TrueNine <truenine304520@gmail.com>
2023-05-17 18:39:38 +0800

    feat 为数据库表增加对树结构的分类树定义，使得其能够存储多个不同的树

--- rds/src/main/resources/db/migration/V1__mysql_helper_fun.sql
+++ rds/src/main/resources/db/migration/V1__mysql_helper_fun.sql
@@ -54,8 +54,10 @@ BEGIN
     'ADD `rln` BIGINT UNSIGNED DEFAULT 1 COMMENT \'左节点 row left node\',',
     'ADD `rrn` BIGINT UNSIGNED DEFAULT 2 COMMENT \'右节点 row right node\',',
     'ADD `nlv` BIGINT UNSIGNED DEFAULT 0 COMMENT \'节点级别 node level\',',
+    'ADD `tgi` BIGINT UNSIGNED DEFAULT 0 COMMENT \'树组id tree group id\',',
     'ADD INDEX(`rln`) COMMENT \'索引左节点\',',
     'ADD INDEX(`rrn`) COMMENT \'索引右节点\',',
+    'ADD INDEX(`tgi`) COMMENT \'树组id\','
     'ADD INDEX(`rpi`) COMMENT \'自联 父节点\';'
     );
   SET @statement = CONCAT(@after);
