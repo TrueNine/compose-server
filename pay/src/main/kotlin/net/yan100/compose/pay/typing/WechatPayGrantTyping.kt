04ee973280d27af73eb5086a7695bf04b920f4ff
TrueNine <truenine304520@gmail.com>
2023-05-29 05:02:38 +0800

    fix 改正命名规则

--- pay/src/main/kotlin/net/yan100/compose/pay/typing/WechatPayGrantTyping.kt
+++ pay/src/main/kotlin/net/yan100/compose/pay/typing/WechatPayGrantTyping.kt
@@ -1,5 +1,6 @@
 package net.yan100.compose.pay.typing
 
+import com.fasterxml.jackson.annotation.JsonValue
 import net.yan100.compose.core.lang.StringTyping
 
 enum class WechatPayGrantTyping(
@@ -7,7 +8,13 @@ enum class WechatPayGrantTyping(
 ) : StringTyping {
   AUTH_CODE("authorization_code");
 
+  @JsonValue
   override fun getValue(): String? {
     return this.typingCode
+  }
+
+  companion object {
+    @JvmStatic
+    fun findVal(v: String?) = WechatPayGrantTyping.values().find { it.typingCode == v }
   }
 }
