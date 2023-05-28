9783ef33e3767357b4d557342e9eb31a68b61c90
TrueNine <truenine304520@gmail.com>
2023-05-29 00:41:08 +0800

    fix恢复 gradle 仓库，并尝试构建

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,10 @@
 pluginManagement {
   repositories {
     mavenLocal()
-//    maven(url = uri("https://maven.aliyun.com/repository/gradle-plugin"))
-//    maven(url = uri("https://maven.aliyun.com/repository/jcenter"))
-//    maven(url = uri("https://maven.aliyun.com/repository/public"))
     maven(url = uri("https://repo.huaweicloud.com/repository/maven/"))
+    maven(url = uri("https://maven.aliyun.com/repository/gradle-plugin"))
+    maven(url = uri("https://maven.aliyun.com/repository/jcenter"))
+    maven(url = uri("https://maven.aliyun.com/repository/public"))
     mavenCentral()
     gradlePluginPortal()
     google()
