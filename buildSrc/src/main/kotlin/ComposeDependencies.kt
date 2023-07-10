b6651a02a8039a5328de13e1acb564b1704eae93
TrueNine <truenine304520@gmail.com>
2023-07-10 13:01:58 +0800

    feat 推进版本号

--- buildSrc/src/main/kotlin/ComposeDependencies.kt
+++ buildSrc/src/main/kotlin/ComposeDependencies.kt
@@ -3,7 +3,7 @@
  */
 object ProjectManager {
   const val group = "net.yan100.compose"
-  const val version = "0.6.25"
+  const val version = "0.6.26"
   const val encoding = "UTF-8"
   const val gradleVersion = "8.2"
 }
