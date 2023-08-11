2a032c9ff013c6efb3ab0a9e9ab820c294556118
TrueNine <truenine304520@gmail.com>
2023-08-11 22:25:29 +0800

    feat 新增测试

--- data-common/data-common-crawler/src/main/kotlin/net/yan100/compose/crawler/playwright/PageFunctions.kt
+++ data-common/data-common-crawler/src/main/kotlin/net/yan100/compose/crawler/playwright/PageFunctions.kt
@@ -49,11 +49,64 @@ fun Page.waitForPageAllLoaded(): Page {
 }
 
 fun Page.locatorWaitFor(
-  selector: String,
-  locatorOptions: Page.LocatorOptions? = null,
-  waitForOptions: WaitForOptions? = WaitForOptions().setState(WaitForSelectorState.ATTACHED)
+  selector: String, locatorOptions: Page.LocatorOptions? = null, waitForOptions: WaitForOptions? = WaitForOptions().setState(WaitForSelectorState.ATTACHED)
 ): Locator {
   val l = this.locator(selector, locatorOptions)
   l.waitFor(waitForOptions)
   return l
+}
+
+fun <T : Any> Page.loopNext(): PageWaitChain<T> {
+  return PageWaitChain(this)
+}
+
+
+class PageWaitChain<T : Any>(
+  private var page: Page
+) {
+  private var initFn: (Page) -> Page? = { it }
+  private var stepFn: (Page) -> Page? = { it }
+  private var waitFn: (Page) -> Page? = { it }
+  private var endFn: (Page) -> Boolean = { true }
+
+  @Suppress("UNCHECKED_CAST")
+  private var exeFn: (Page) -> T = { Unit as T }
+
+  fun init(
+    i: (Page) -> Page?
+  ): PageWaitChain<T> {
+    this.initFn = i
+    return this
+  }
+
+  fun execute(
+    exe: (Page) -> T
+  ): PageWaitChain<T> {
+    this.exeFn = exe
+    return this
+  }
+
+  fun waitForStep(
+    wait: (Page) -> Page?
+  ): PageWaitChain<T> {
+    this.waitFn = wait
+    return this
+  }
+
+  fun endWith(
+    end: (Page) -> Boolean
+  ): PageWaitChain<T> {
+    this.endFn = end
+    return this
+  }
+
+  fun run(): List<T> {
+    val result = mutableListOf<T>()
+    this.page = initFn(this.page) ?: return result
+    do {
+      this.page = waitFn(page) ?: return result
+      result += exeFn(page)
+    } while (!endFn(page))
+    return result
+  }
 }
--- data-common/data-common-crawler/src/test/kotlin/net/yan100/compose/crawler/t/T1.kt
+++ data-common/data-common-crawler/src/test/kotlin/net/yan100/compose/crawler/t/T1.kt
@@ -5,6 +5,8 @@ import com.microsoft.playwright.options.*
 import net.yan100.compose.crawler.playwright.*
 import org.testng.annotations.Test
 import java.nio.file.Paths
+import java.time.LocalDateTime
+import kotlin.random.Random
 
 class T1 {
   @Test
@@ -14,31 +16,32 @@ class T1 {
         browser.withActions(
           Browser.NewContextOptions().setColorScheme(ColorScheme.LIGHT).setViewportSize(1920, 1080),
         ) { _, page ->
-          page.navigate("https://www.bilibili.com/")
-          page.querySelector("#nav-searchform div.nav-search-content input.nav-search-input").fill("我的滑板鞋")
-          page.waitForPopup {
-            page.locator("#nav-searchform").getByRole(AriaRole.IMG).nth(1).click()
-          }.use { searchPage ->
-            searchPage.locatorWaitFor("//span[@class='vui_tabs--nav-text' and text() = '视频']").click()
-            searchPage.waitForPageAllLoaded()
-            var next: Boolean
-            var step = 0
-            do {
-              searchPage.waitForPageAllLoaded()
-              val text = searchPage.querySelector("//div[contains(@class,'video-list')]").querySelectorAll("//div[contains(@class,'video-list-item')]")
-                .map { it.innerText() }
-              println(text)
-              val btn = searchPage.locatorWaitFor("//button[contains(@class ,'vui_pagenation--btn') and contains(text(),'下一页')]")
-              val disabled = btn.getAttribute("disabled")
-              next = disabled == null
-
-              searchPage.screenshot(
-                Page.ScreenshotOptions().setFullPage(true).setType(ScreenshotType.PNG).setPath(Paths.get("example${step}.png"))
+          val locals = page.loopNext<Any>()
+            .init {
+              it.navigate("https://www.bilibili.com/")
+              it.querySelector("#nav-searchform div.nav-search-content input.nav-search-input").fill("我的滑板鞋")
+              val newPage = it.waitForPopup {
+                it.locatorWaitFor("#nav-searchform").getByRole(AriaRole.IMG).nth(1).click()
+              }
+              newPage.locatorWaitFor("//span[contains(@class,'vui_tabs--nav-text') and text()='视频']").click()
+              newPage
+            }.execute {
+              val b = it.screenshot(
+                Page.ScreenshotOptions().setFullPage(true).setType(ScreenshotType.PNG).setPath(Paths.get("example-${LocalDateTime.now().nano}.png"))
               )
-              step += 1
-              if (next) btn.click()
-            } while (next)
-          }
+              it.localStorage()
+            }.endWith {
+              val btn = it.locatorWaitFor("//button[contains(@class ,'vui_pagenation--btn') and contains(text(),'下一页')]")
+              val disabled = btn.getAttribute("disabled")
+              val next = disabled == null
+              if (next) {
+                btn.click()
+                it.waitForPageAllLoaded()
+              }
+              !next
+            }
+            .run()
+          println(locals)
         }
       }
     }
