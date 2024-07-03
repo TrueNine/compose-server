76f31b12405bac520c3e39f41dbd055062ec799d
赵日天 <truenine304520@gmail.com>
2024-07-03 19:09:05 +0800

    fix 更新依赖统计

--- security/security-oauth2/build.gradle.kts
+++ security/security-oauth2/build.gradle.kts
@@ -1,10 +1,11 @@
 project.version = libs.versions.compose.asProvider().get()
 
 dependencies {
-  implementation(libs.spring.security.core)
   implementation(project(":core"))
   implementation(project(":depend:depend-web-client"))
-  testImplementation(libs.spring.boot.web)
+
+  implementation(libs.org.springframework.security.springSecurityCore)
+  testImplementation(libs.org.springframework.boot.springBootStarterWeb)
 }
 
 publishing {
