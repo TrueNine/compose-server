1fbf9abcb0b2f8fd512a16671e359ecc5ef5e261
赵日天 <truenine304520@gmail.com>
2024-07-11 20:17:18 +0800

    feat 增加 账号和用户信息的分组

--- rds/src/main/kotlin/net/yan100/compose/rds/repositories/IUsrRepo.kt
+++ rds/src/main/kotlin/net/yan100/compose/rds/repositories/IUsrRepo.kt
@@ -19,15 +19,12 @@ package net.yan100.compose.rds.repositories
 import java.time.LocalDateTime
 import kotlinx.coroutines.*
 import net.yan100.compose.core.alias.RefId
-import net.yan100.compose.core.alias.TODO
 import net.yan100.compose.rds.entities.account.FullUsr
 import net.yan100.compose.rds.entities.account.Usr
-import net.yan100.compose.rds.entities.info.UserInfo
 import net.yan100.compose.rds.repositories.base.IRepo
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
-import org.springframework.transaction.annotation.Transactional
 
 @Repository
 interface IUsrRepo : IRepo<Usr> {
@@ -115,80 +112,4 @@ interface IUsrRepo : IRepo<Usr> {
 @Repository
 interface IFullUserRepo : IRepo<FullUsr> {
   fun findByAccount(account: String): FullUsr?
-}
-
-@Repository
-interface IUserInfoRepo : IRepo<UserInfo> {
-  fun existsAllByIdAndIdCardIsNotNull(id: RefId): Boolean
-
-  fun existsAllByIdAndPhoneIsNotNull(id: RefId): Boolean
-
-  fun existsAllByIdAndFirstNameIsNotNull(id: RefId): Boolean
-
-  fun existsAllByIdAndLastNameIsNotNull(id: RefId): Boolean
-
-  suspend fun existsByIdAndIsRealPeople(id: RefId): Boolean =
-    withContext(Dispatchers.IO) {
-      if (!existsById(id)) false
-      else
-        listOf(
-            async { existsAllByIdAndIdCardIsNotNull(id) },
-            async { existsAllByIdAndPhoneIsNotNull(id) },
-            async { existsAllByIdAndFirstNameIsNotNull(id) },
-            async { existsAllByIdAndLastNameIsNotNull(id) }
-          )
-          .awaitAll()
-          .all { it }
-    }
-
-  @Query("""
-    select count(i.id)
-    from UserInfo i
-    join Usr u on u.id = i.userId
-  """) fun countAllByHasUser(): Long
-
-  fun existsAllByPhone(phone: String): Boolean
-
-  fun findAllByPhone(phone: String): List<UserInfo>
-
-  fun existsAllByIdCard(idCard: String): Boolean
-
-  fun findAllByIdCard(idCard: String): List<UserInfo>
-
-  @Query("""
-    select i.id
-    from UserInfo i
-    where i.userId = :userId
-  """) fun findAllIdByUserId(userId: RefId): List<RefId>
-
-  @Query("""
-    select i.userId
-    from UserInfo i
-    where i.id = :id
-  """) fun findUserIdById(id: RefId): RefId?
-
-  @TODO("可会查询出多个用户") fun findByUserId(userId: RefId): UserInfo?
-
-  fun findFirstByUserIdAndPriIsTrue(userId: RefId): UserInfo?
-
-  /** 根据 微信 openId 查询对应 User */
-  @Query("""
-    from Usr u
-    left join UserInfo i ON u.id = i.userId
-    where i.wechatOpenid = :openid
-    """)
-  fun findUserByWechatOpenId(openid: String): Usr?
-
-  /** 根据 电话号码查询用户手机号 */
-  @Query("""
-    from Usr u
-    left join UserInfo i on u.id = i.userId
-    where i.phone = :phone
-  """) fun findUserByPhone(phone: String): Usr?
-
-  fun existsByPhone(phone: String): Boolean
-
-  fun existsByWechatOpenid(wechatOpenId: String): Boolean
-
-  @Transactional(rollbackFor = [Exception::class]) fun deleteByPhone(phone: String): Int
 }
