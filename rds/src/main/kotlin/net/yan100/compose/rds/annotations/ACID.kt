15b4f8fd9c0425c83e5a90318243f86917ba4453
赵日天 <truenine304520@gmail.com>
2024-09-11 01:45:37 +0800

    feat(ACID): 事务注解别名

---
+++ rds/src/main/kotlin/net/yan100/compose/rds/annotations/ACID.kt
@@ -0,0 +1,23 @@
+package net.yan100.compose.rds.annotations
+
+import jakarta.transaction.Transactional
+import java.lang.annotation.Inherited
+
+/**
+ * ## spring 中 事务注解 别称
+ *
+ * 对齐 默认加上了 Exception Throwable Error
+ * @author TrueNine
+ * @since 2024-09-11
+ * @see [Transactional]
+ * @see [Exception]
+ * @see [Throwable]
+ * @see [Error]
+ */
+@Inherited
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Transactional(rollbackOn = [Exception::class, Throwable::class, Error::class])
+annotation class ACID
+
