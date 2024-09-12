c403eca4acf011a76c518a725d57e28e9953c01a
赵日天 <truenine304520@gmail.com>
2024-09-12 22:03:32 +0800

    style: 重构格式

--- rds/src/main/kotlin/net/yan100/compose/rds/RdsEntrance.kt
+++ rds/src/main/kotlin/net/yan100/compose/rds/RdsEntrance.kt
@@ -17,12 +17,9 @@
 package net.yan100.compose.rds
 
 import net.yan100.compose.rds.autoconfig.AutoConfigEntrance
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Import
 
-@Import(AutoConfigEntrance::class) @SpringBootApplication internal class RdsEntrance
-
-internal fun main(args: Array<String>) {
-  SpringApplication.run(RdsEntrance::class.java, *args)
-}
+@Import(AutoConfigEntrance::class)
+@SpringBootApplication
+internal class RdsEntrance
