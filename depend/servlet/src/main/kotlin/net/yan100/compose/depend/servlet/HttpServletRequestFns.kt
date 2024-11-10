cf0d6636ebf12e3d542a13e8914e5c5ea2d424e8
赵日天 <truenine304520@gmail.com>
2024-11-10 13:04:49 +0800

    refactor(servlet): 归类方法的所属

---
+++ depend/servlet/src/main/kotlin/net/yan100/compose/depend/servlet/HttpServletRequestFns.kt
@@ -0,0 +1,20 @@
+package net.yan100.compose.depend.servlet
+
+import jakarta.servlet.http.HttpServletRequest
+import net.yan100.compose.core.consts.IHeaders
+import net.yan100.compose.core.consts.IInterAddr
+
+val HttpServletRequest.headerMap: Map<String, String>
+  get() = headerNames.asSequence().map { it to getHeader(it) }.toMap()
+
+/** 获取当前设备的 deviceId */
+val HttpServletRequest.deviceId: String? get() = IHeaders.getDeviceId(this)
+
+
+/**
+ * ## 当前请求的 ip 地址
+ *
+ * 尽量获取到真实的ip地址
+ */
+val HttpServletRequest.remoteRequestIp: String
+  get() = IInterAddr.getRequestIpAddress(this)
