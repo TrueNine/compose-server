bf1836c2371c854ae85a6af1a2b33322da68156d
赵日天 <truenine304520@gmail.com>
2024-11-28 11:26:31 +0800

    build:重构项目结构配置以提高可维护性
    - 使用循环替代重复的 include 和 findProject调用
    - 优化了 rds、data、security、depend 和 ksp 模块的配置
    - 调整了 gradle-plugin 的加载顺序

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,80 +8,67 @@ rootProject.name = "compose-server"
 includeBuild("version-catalog")
 
 
-include("gradle-plugin")
+
 findProject(":gradle-plugin")?.name = "gradle-plugin"
 
-include("core")
-findProject(":core")?.name = "core"
+listOf(
+  "test-toolkit",
+  "core",
+  "gradle-plugin",
+  "oss",
+  "cacheable",
+  "cacheable",
+  "pay"
+).forEach { p ->
+  include(p)
+  findProject(":$p")?.name = p
+}
 
-
-include("oss")
-findProject(":oss")?.name = "oss"
-
-
-include("test-toolkit")
-findProject(":test-toolkit")?.name = "test-toolkit"
-
-include("cacheable")
-findProject(":cacheable")?.name = "cacheable"
-
-
-
-include("pay")
-findProject(":pay")?.name = "pay"
-
-
-// 关系型数据库
-include("rds:crud")
-findProject(":rds:crud")?.name = "rds-crud"
-include("rds:core")
-findProject(":rds:core")?.name = "rds-core"
-include("rds:jimmer")
-findProject(":rds:jimmer")?.name = "rds-jimmer"
-include("rds:migration-mysql")
-findProject(":rds:migration-mysql")?.name = "rds-migration-mysql"
-include("rds:migration-postgres")
-findProject(":rds:migration-postgres")?.name = "rds-migration-postgres"
+("rds" to listOf(
+  "core", "crud", "jimmer", "migration-mysql", "migration-postgres"
+)).apply {
+  second.forEach { n ->
+    include("$first:$n")
+    findProject(":$first:$n")?.name = "$first-$n"
+  }
+}
 
 // 数据采集器
-include("data")
-findProject(":data")?.name = "data"
-include("data:crawler")
-findProject(":data:crawler")?.name = "data-crawler"
-include("data:extract")
-findProject(":data:extract")?.name = "data-extract"
+("data" to listOf(
+  "crawler", "extract"
+)).apply {
+  second.forEach { n ->
+    include("$first:$n")
+    findProject(":$first:$n")?.name = "$first-$n"
+  }
+}
 
 // 安全相关
-include("security:spring")
-findProject(":security:spring")?.name = "security-spring"
-include("security:oauth2")
-findProject(":security:oauth2")?.name = "security-oauth2"
-include("security:crypto")
-findProject(":security:crypto")?.name = "security-crypto"
-
+("security" to listOf(
+  "spring", "oauth2", "crypto"
+)).apply {
+  second.forEach { n ->
+    include("$first:$n")
+    findProject(":$first:$n")?.name = "$first-$n"
+  }
+}
 
 // 特定依赖处理
-include("depend")
-findProject(":depend")?.name = "depend"
-include("depend:servlet")
-findProject(":depend:servlet")?.name = "depend-servlet"
-include("depend:paho")
-findProject(":depend:paho")?.name = "depend-paho"
-include("depend:http-exchange")
-findProject(":depend:http-exchange")?.name = "depend-http-exchange"
-include("depend:jsr303-validation")
-findProject(":depend:jsr303-validation")?.name = "depend-jsr303-validation"
-include("depend:jackson")
-findProject(":depend:jackson")?.name = "depend-jackson"
-include("depend:springdoc-openapi")
-findProject(":depend:springdoc-openapi")?.name = "depend-springdoc-openapi"
-include(":depend:xxl-job")
-findProject(":depend:xxl-job")?.name = "depend-xxl-job"
+("depend" to listOf(
+  "servlet", "paho", "http-exchange", "jsr303-validation", "jackson", "springdoc-openapi", "xxl-job"
+)).apply {
+  second.forEach { n ->
+    include("$first:$n")
+    findProject(":$first:$n")?.name = "$first-$n"
+  }
+}
 
 // ksp
-include("ksp:plugin")
-findProject(":ksp:plugin")?.name = "ksp-plugin"
-include("ksp:core")
-findProject(":ksp:core")?.name = "ksp-core"
-include("ksp:toolkit")
-findProject(":ksp:toolkit")?.name = "ksp-toolkit"
+("ksp" to listOf(
+  "plugin", "core", "toolkit"
+)).apply {
+  second.forEach { n ->
+    include("$first:$n")
+    findProject(":$first:$n")?.name = "$first-$n"
+  }
+}
