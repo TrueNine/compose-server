247b1f094c07e5f26802246678da1204bb872ac4
赵日天 <truenine304520@gmail.com>
2024-12-01 15:39:51 +0800

    refactor(rds-core): 重构 IAnyEntity 类
    - 将 IAnyEntity 从抽象类改为接口
    - 移除了不必要的属性和方法
    - 简化了实体类的结构，提高了灵活性
    - 删除了未使用的注解和导入

--- rds/core/src/main/kotlin/net/yan100/compose/rds/core/entities/IAnyEntity.kt
+++ rds/core/src/main/kotlin/net/yan100/compose/rds/core/entities/IAnyEntity.kt
@@ -16,22 +16,16 @@
  */
 package net.yan100.compose.rds.core.entities
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
-import jakarta.persistence.Transient
 import net.yan100.compose.core.Id
-import net.yan100.compose.core.bool
 import net.yan100.compose.core.consts.IDbNames
-import net.yan100.compose.core.domain.AbstractLazyInitScope
 import net.yan100.compose.core.domain.ISensitivity
 import net.yan100.compose.rds.core.listener.BizCodeInsertListener
 import net.yan100.compose.rds.core.listener.SnowflakeIdInsertListener
-import org.hibernate.Hibernate
 import org.springframework.data.domain.Persistable
-import java.io.Serial
 import java.io.Serializable
 
 /**
@@ -46,91 +40,42 @@ import java.io.Serializable
   BizCodeInsertListener::class,
   SnowflakeIdInsertListener::class,
 )
-abstract class IAnyEntity : ISensitivity,
+interface IAnyEntity : ISensitivity,
   Persistable<Id>,
   IExtensionDefineScope,
-  IEnhanceEntity, Serializable,
-  AbstractLazyInitScope() {
+  IEnhanceEntity, Serializable {
   companion object {
-    @Serial
-    const val serialVersionUID = 1L
 
     /** 主键 */
-    @kotlin.jvm.Transient
     const val ID = IDbNames.ID
+  }
+
+  /** id */
+  fun setId(id: Id) {
+    throw NotImplementedError("entity not implement primary id set function")
   }
 
   /** id */
   @jakarta.persistence.Id
   @Column(name = ID)
-  @Schema(
-    title = ID,
-    name = ID,
-    description = "仅在更新时需要携带 ID",
-    example = "7173087959242248192",
-    examples = ["7001234523405", "7001234523441"],
-    required = false,
-    requiredMode = Schema.RequiredMode.NOT_REQUIRED,
-  )
-  @Suppress("ALL")
-  private var id: Id? = null
-    @Transient @JsonIgnore @JvmName("_\$\$_get_kotlin_internal_primary_id") get() = field ?: ""
+  override fun getId(): Id = throw NotImplementedError("entity not implement primary id get function")
 
-  @Schema(required = false, requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  open fun setId(id: String) {
-    this.id = id
+  fun toNewEntity() {
+    id = ""
   }
 
-  override fun getId(): Id = this.id!!
 
-  override fun equals(other: Any?): Boolean {
-    return if (this === other) true
-    else if (null == other || Hibernate.getClass(this) != Hibernate.getClass(other)) false
-    else !isNew && id == (other as IAnyEntity).id
+  override fun isNew(): Boolean {
+    return "" == id || "null" == id
   }
 
-  override fun toString(): String {
-    return if (isNew) "AnyEntity(New)" else "AnyEntity(id=$id)"
+  override fun recordChangedSensitiveData() {
   }
-
-  override fun hashCode(): Int {
-    return javaClass.hashCode()
-  }
-
-  @Transient
-  @JsonIgnore
-  @Schema(hidden = true)
-  open fun toNewEntity() {
-    id = null
-  }
-
-  @Transient
-  @JsonIgnore
-  @Schema(hidden = true)
-  final override fun isNew(): Boolean {
-    return null == id || "" == id || "null" == id
-  }
-
-  @kotlin.jvm.Transient
-  @Transient
-  @JsonIgnore
-  @Schema(hidden = true)
-  private var _sensed: Boolean = false
-
-  final override fun recordChangedSensitiveData() {
-    _sensed = true
-  }
-
-  @kotlin.jvm.Transient
-  @Transient
-  @JsonIgnore
-  @Schema(hidden = true)
-  final override val isChangedToSensitiveData: bool = _sensed
 
   override fun changeWithSensitiveData() {
     super.changeWithSensitiveData()
-    require(!_sensed) { "数据已经脱敏，无需重复执行" }
-    this.id = null
+    require(!isChangedToSensitiveData) { "数据已经脱敏，无需重复执行" }
+    this.id = ""
     recordChangedSensitiveData()
   }
 }
