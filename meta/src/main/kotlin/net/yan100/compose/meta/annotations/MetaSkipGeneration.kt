bc5b53cd72b80cb183f4a695bd184f49c11b6704
赵日天 <truenine304520@gmail.com>
2024-12-01 15:34:47 +0800

    feat(annotations): 添加 MetaSkipGeneration 注解
    ## 指示生成器，该元素应被丢弃
    
    这是一个兼容性设计，因为目前 开发的 ksp 插件对多轮编译支持不佳。
    被注解后，该元素将被丢弃，随后，丢弃的元素将不会参与到后续的生成当中。

---
+++ meta/src/main/kotlin/net/yan100/compose/meta/annotations/MetaSkipGeneration.kt
@@ -0,0 +1,25 @@
+package net.yan100.compose.meta.annotations
+
+import java.lang.annotation.Inherited
+
+/**
+ * ## 指示生成器，该元素应被丢弃
+ *
+ * 这是一个兼容性设计，因为目前 开发的 ksp 插件对多轮编译支持不佳。
+ * 被注解后，该元素将被丢弃，随后，丢弃的元素将不会参与到后续的生成当中。
+ *
+ * @since 2024-12-01
+ * @author TrueNine
+ */
+@Target(
+  AnnotationTarget.PROPERTY,
+  AnnotationTarget.PROPERTY_GETTER,
+  AnnotationTarget.PROPERTY_SETTER,
+  AnnotationTarget.FUNCTION,
+  AnnotationTarget.FILE,
+  AnnotationTarget.TYPE
+)
+@Inherited
+@MustBeDocumented
+@Retention(AnnotationRetention.BINARY)
+annotation class MetaSkipGeneration
