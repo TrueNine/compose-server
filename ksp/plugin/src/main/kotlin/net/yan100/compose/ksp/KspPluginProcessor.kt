2b3472ff29fbf01c6af5fe69142cb39a4ab744b2
赵日天 <truenine304520@gmail.com>
2024-12-02 02:23:01 +0800

    feat(ksp): 支持实体监听器的元数据定义处理- 增加对 @EntityListener 注解的处理逻辑 - 优化元数据定义的处理流程，提高代码的可扩展性和可维护性 - 修复了一些与注解处理相关的潜在问题

--- ksp/plugin/src/main/kotlin/net/yan100/compose/ksp/KspPluginProcessor.kt
+++ ksp/plugin/src/main/kotlin/net/yan100/compose/ksp/KspPluginProcessor.kt
@@ -16,10 +16,7 @@
  */
 package net.yan100.compose.ksp
 
-import com.google.devtools.ksp.KspExperimental
-import com.google.devtools.ksp.getAnnotationsByType
-import com.google.devtools.ksp.getDeclaredProperties
-import com.google.devtools.ksp.isAnnotationPresent
+import com.google.devtools.ksp.*
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
@@ -27,8 +24,9 @@ import com.google.devtools.ksp.symbol.ClassKind
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSDeclaration
+import com.squareup.kotlinpoet.ksp.toAnnotationSpec
 import net.yan100.compose.ksp.toolkit.models.DeclarationContext
-import net.yan100.compose.ksp.toolkit.simpleNameAsStringStr
+import net.yan100.compose.ksp.toolkit.simpleNameAsString
 import net.yan100.compose.ksp.visitor.JpaNameClassVisitor
 import net.yan100.compose.ksp.visitor.RepositoryIPageExtensionsVisitor
 import net.yan100.compose.meta.annotations.MetaDef
@@ -43,23 +41,35 @@ class KspPluginProcessor(
 
   @OptIn(KspExperimental::class)
   override fun process(resolver: Resolver): List<KSAnnotated> {
-    resolver.getSymbolsWithAnnotation("net.yan100.compose.meta.annotations.MetaDef")
+    val lis = resolver.getSymbolsWithAnnotation(
+      "jakarta.persistence.EntityListener"
+    ).filterIsInstance<KSDeclaration>().firstOrNull()?.let {
+      it.annotations.firstOrNull()?.toAnnotationSpec()
+    }
+    val jpaSymbols = resolver.getSymbolsWithAnnotation("net.yan100.compose.meta.annotations.MetaDef")
+    val nextSymbols = jpaSymbols.filter { !it.validate() }.toList()
+    jpaSymbols
+      .filter { it.validate() }
       .filterIsInstance<KSClassDeclaration>()
-      //.filter { it.isAbstract() }
+      .filterNot { nextSymbols.contains(it) }
       .filter { !it.isAnnotationPresent(MetaSkipGeneration::class) }
       .filter { it.getDeclaredProperties().toList().isNotEmpty() }
       .filter {
         !it.isCompanionObject
       }
-      .filter { it.simpleNameAsStringStr.startsWith("Super") }
-      .filterNot { it.simpleNameAsStringStr.contains("$") }
+      .filter { it.simpleNameAsString.startsWith("Super") }
+      .filterNot { it.simpleNameAsString.contains("$") }
       .filter { it.getAnnotationsByType(MetaDef::class).toList().isNotEmpty() }
-      .forEach { getCtxData(it, resolver).accept(JpaNameClassVisitor()) }
+      .forEach {
+        getCtxData(it, resolver).accept(
+          JpaNameClassVisitor(lis)
+        )
+      }
 
     resolver.getPackagesWithAnnotation("org.springframework.stereotype.Repository")
       .filterIsInstance<KSClassDeclaration>()
       .filter { it.classKind == ClassKind.INTERFACE }
       .forEach { getCtxData(it, resolver).accept(RepositoryIPageExtensionsVisitor()) }
-    return emptyList()
+    return nextSymbols
   }
 }
