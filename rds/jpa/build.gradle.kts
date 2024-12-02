f50e3c02de9e688fef4d6cf53ee1e7cd893b1f2f
赵日天 <truenine304520@gmail.com>
2024-12-02 21:40:40 +0800

    build: 更新 RDS JPA模块的构建配置
    -重命名构建文件：从 rds/crud/build.gradle.kts 改为 rds/jpa/build.gradle.kts
    - 更新版本号引用：从 composeRdsCrud 改为 composeRdsJpa
    - 添加 KSP 插件配置，用于处理 JPA 注解
    -移除未使用的依赖注释

--- rds/jpa/build.gradle.kts
+++ rds/jpa/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
   alias(libs.plugins.com.google.devtools.ksp)
 }
 
-version = libs.versions.composeRdsCrud.get()
+version = libs.versions.composeRdsJpa.get()
 
 kapt {
   correctErrorTypes = true
@@ -11,13 +11,15 @@ kapt {
   arguments { arg("plugin", "com.querydsl.apt.jpa.JPAAnnotationProcessor") }
 }
 
+ksp {
+  arg("net.yan100.compose.ksp.plugin.generateJpa", "true")
+}
+
 dependencies {
   implementation(libs.org.jetbrains.kotlinx.kotlinxCoroutinesCore)
 
   api(libs.org.springframework.boot.springBootStarterDataJpa)
   implementation(libs.org.hibernate.orm.hibernateCore)
-
-  //implementation(libs.jakarta.validation.jakartaValidationApi)
 
   implementation(libs.org.springframework.security.springSecurityCrypto)
 
