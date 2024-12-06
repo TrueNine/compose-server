08827156533d151968695f8b93941149fa09f5dc
赵日天 <truenine304520@gmail.com>
2024-12-06 19:15:04 +0800

    test(rds/crud): 添加附件枚举类型保存测试
    - 在 IJimmerAttachmentRepoTest 中添加了测试函数 ensureSaveEnumTypeOk
    - 测试插入一个带有枚举类型 BASE_URL 的附件记录- 验证插入后的记录枚举类型是否正确

--- rds/crud/src/test/kotlin/net/yan100/compose/rds/crud/repositories/jimmer/IJimmerAttachmentRepoTest.kt
+++ rds/crud/src/test/kotlin/net/yan100/compose/rds/crud/repositories/jimmer/IJimmerAttachmentRepoTest.kt
@@ -1,11 +1,14 @@
 package net.yan100.compose.rds.crud.repositories.jimmer
 
 import jakarta.annotation.Resource
+import net.yan100.compose.rds.core.typing.AttachmentTyping
+import net.yan100.compose.rds.crud.entities.jimmer.Attachment
 import net.yan100.compose.testtookit.RDBRollback
 import net.yan100.compose.testtookit.log
 import org.springframework.boot.test.context.SpringBootTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 @RDBRollback
@@ -28,5 +31,16 @@ class IJimmerAttachmentRepoTest {
     val atts = attRepo.findFilesBy()
     log.info("selected att size: {}", atts.size)
     assertNotNull(atts)
+  }
+
+  @Test
+  fun `ensure save enum type ok`() {
+    val saved = attRepo.insert(Attachment {
+      attType = AttachmentTyping.BASE_URL
+    })
+    assertEquals(
+      AttachmentTyping.BASE_URL,
+      saved.attType
+    )
   }
 }
