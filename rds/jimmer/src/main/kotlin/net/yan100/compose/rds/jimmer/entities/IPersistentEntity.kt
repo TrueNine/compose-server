2a8f058621333a80e6cd6507c8cd963f1a1b32d0
赵日天 <truenine304520@gmail.com>
2024-12-10 21:28:56 +0800

    feat(rds-jimmer): 使用 Long 类型生成 ID 并转换为 String
    - 将 ID 生成器从 JimmerSnowflakeStringIdGenerator 更改为 JimmerSnowflakeLongIdGenerator
    - 在 ID 字段上添加 JimmerLongToStringConverter转换器
    - 这种改动可以提高 ID 生成的效率，并减少存储空间的占用

--- rds/jimmer/src/main/kotlin/net/yan100/compose/rds/jimmer/entities/IPersistentEntity.kt
+++ rds/jimmer/src/main/kotlin/net/yan100/compose/rds/jimmer/entities/IPersistentEntity.kt
@@ -1,7 +1,9 @@
 package net.yan100.compose.rds.jimmer.entities
 
 import net.yan100.compose.core.RefId
-import net.yan100.compose.rds.jimmer.generators.JimmerSnowflakeStringIdGenerator
+import net.yan100.compose.rds.core.converters.jimmer.JimmerLongToStringConverter
+import net.yan100.compose.rds.jimmer.generators.JimmerSnowflakeLongIdGenerator
+import org.babyfish.jimmer.jackson.JsonConverter
 import org.babyfish.jimmer.sql.GeneratedValue
 import org.babyfish.jimmer.sql.Id
 import org.babyfish.jimmer.sql.MappedSuperclass
@@ -10,6 +12,7 @@ import org.babyfish.jimmer.sql.MappedSuperclass
 @MappedSuperclass
 interface IPersistentEntity {
   @Id
-  @GeneratedValue(generatorRef = JimmerSnowflakeStringIdGenerator.JIMMER_SNOWFLAKE_STRING_ID_GENERATOR_NAME)
+  @JsonConverter(JimmerLongToStringConverter::class)
+  @GeneratedValue(generatorRef = JimmerSnowflakeLongIdGenerator.JIMMER_SNOWFLAKE_LONG_ID_GENERATOR_NAME)
   val id: RefId
 }
