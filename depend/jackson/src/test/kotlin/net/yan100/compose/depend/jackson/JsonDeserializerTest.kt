cc5816a99b5b1161eb83741da691598918d678b1
赵日天 <truenine304520@gmail.com>
2024-12-11 07:44:57 +0800

    test(depend-jackson): 添加长整型序列化测试并调整日期时间序列化方法
    - 新增 serializeLongToString 测试函数，验证长整型数值的 JSON 序列化
    - 重命名 testLocalDatetime 为 serializeLocalDateTime，优化测试函数命名
    - 更新测试文件结构，改善代码可读性和维护性

--- depend/jackson/src/test/kotlin/net/yan100/compose/depend/jackson/JsonDeserializerTest.kt
+++ depend/jackson/src/test/kotlin/net/yan100/compose/depend/jackson/JsonDeserializerTest.kt
@@ -17,25 +17,31 @@
 package net.yan100.compose.depend.jackson
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import jakarta.annotation.Resource
 import net.yan100.compose.core.toDate
 import net.yan100.compose.core.toLocalDatetime
 import net.yan100.compose.core.toLong
 import net.yan100.compose.testtookit.log
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import java.time.LocalDateTime
 import java.util.*
+import kotlin.test.Test
 import kotlin.test.assertEquals
 
 @SpringBootTest
 class JsonDeserializerTest {
-
-  @Resource
-  lateinit var mapper: ObjectMapper
+  @JsonSerialize
+  lateinit var mapper: ObjectMapper @Resource set
 
   @Test
-  fun testLocalDatetime() {
+  fun serializeLongToString() {
+    val longJsonString = mapper.writeValueAsString(1234567890123451L)
+    assertEquals("1234567890123451", longJsonString, "正常情况下，未注册 string 则必须转换为 long 数字格式")
+  }
+
+  @Test
+  fun serializeLocalDateTime() {
     val localDatetime = Date().toLocalDatetime()
     val json = mapper.writeValueAsString(localDatetime)
     log.info(json)
