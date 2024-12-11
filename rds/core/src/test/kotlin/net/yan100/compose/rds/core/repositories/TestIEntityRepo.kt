4aa549dd957f858fcaadf0e2af4eabacf3a2139a
赵日天 <truenine304520@gmail.com>
2024-12-12 07:45:58 +0800

    test: 删除 RDS 模块的测试代码
    - 移除了多个测试类和实体类，包括 Ae、TestIEntity 等
    - 删除了相关的测试用例和测试配置
    - 清理了测试代码所在的包结构

--- rds/core/src/test/kotlin/net/yan100/compose/rds/core/repositories/TestIEntityRepo.kt
+++
@@ -1,9 +0,0 @@
-package net.yan100.compose.rds.core.repositories
-
-import net.yan100.compose.rds.core.IRepo
-import net.yan100.compose.rds.core.entities.TestIEntity
-import org.springframework.stereotype.Repository
-
-@Repository
-interface TestIEntityRepo : IRepo<TestIEntity> {
-}
