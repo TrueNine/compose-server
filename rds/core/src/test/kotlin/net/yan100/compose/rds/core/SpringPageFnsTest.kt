44e72e2c36e4c703f5e146c47dd1f961aab80ef7
赵日天 <truenine304520@gmail.com>
2024-12-13 07:47:56 +0800

    test(rds-core): 添加对空 Spring 页面转换的测试用例- 新增测试用例验证空 Spring 页面转换为 PR 的情况- 优化了测试用例的断言顺序和结构 -修复了 unpaged 情况下的比较逻辑

--- rds/core/src/test/kotlin/net/yan100/compose/rds/core/SpringPageFnsTest.kt
+++ rds/core/src/test/kotlin/net/yan100/compose/rds/core/SpringPageFnsTest.kt
@@ -3,6 +3,7 @@ package net.yan100.compose.rds.core
 import net.yan100.compose.core.Pq
 import net.yan100.compose.testtookit.log
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import kotlin.test.Test
@@ -11,6 +12,31 @@ import kotlin.test.assertIs
 import kotlin.test.assertTrue
 
 class SpringPageFnsTest {
+  @Test
+  fun `success convert to pr`() {
+    val springPage = PageImpl(listOf(1, 2, 3), Pq[0, 42].toPageable(), 3)
+    assertEquals(3, springPage.content.size)
+    assertEquals(42, springPage.size)
+    assertEquals(1, springPage.totalPages)
+    assertEquals(3, springPage.totalElements)
+
+    val converted = springPage.toPr()
+    assertEquals(3, converted.d.size)
+    assertEquals(3, converted.t)
+    assertEquals(1, converted.p)
+    assertEquals(null, converted.o)
+    assertEquals(null, converted.pageParam)
+  }
+
+  @Test
+  fun `success convert empty spring page to pr`() {
+    val springPage = Page.empty<Any>()
+    val pr = springPage.toPr()
+    assertEquals(0, pr.d.size)
+    assertEquals(0, pr.t)
+    assertEquals(0, pr.p)
+    assertEquals(null, pr.pageParam)
+  }
 
   @Test
   fun `un paged not exception`() {
@@ -24,7 +50,7 @@ class SpringPageFnsTest {
     val upq = Pq.empty()
     val pg = upq.toPageable()
     val uclass = Pageable.unpaged()::class
-    assertEquals(pg::class, uclass)
+    assertEquals(uclass, pg::class)
     assertTrue(pg.isUnpaged)
     val uResult = Page.empty<Any>(pg).toPr()
     log.info("uResult: {}", uResult)
