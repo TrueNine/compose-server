10314beb9ee18653010e9d30b4b088bcbbc87f78
赵日天 <truenine304520@gmail.com>
2024-12-13 10:30:58 +0800

    refactor(core): 简化 IPageParamLike 接口- 移除了不必要的 Jackson 注解 - 删除了冗余的 get 方法 - 优化了属性定义，提高了代码可读性

--- core/src/main/kotlin/net/yan100/compose/core/domain/IPageParamLike.kt
+++ core/src/main/kotlin/net/yan100/compose/core/domain/IPageParamLike.kt
@@ -1,6 +1,5 @@
 package net.yan100.compose.core.domain
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import net.yan100.compose.core.bool
 import net.yan100.compose.core.i32
 import net.yan100.compose.core.i64
@@ -11,18 +10,16 @@ import net.yan100.compose.core.i64
  */
 interface IPageParamLike {
   /** ## 分页 页面 大小 */
-  val s: i32? get() = null
+  val s: i32?
 
   /**
    * ## 分页 页面 偏移量
    */
-  val o: i64? get() = null
+  val o: i64?
 
   /**
    * ## UnPaged（禁用分页）
    * 默认 false
    */
   val u: bool?
-    @JsonIgnore
-    get() = false
 }
