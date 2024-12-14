8583d1e15919834cf4c642522bd97d3136606faf
赵日天 <truenine304520@gmail.com>
2024-12-15 07:21:17 +0800

    feat(orm): 添加计算属性注解 MetaFormula
    - 新增 MetaFormula 注解，用于表示计算属性
    - 该注解可以应用于属性、属性的 getter 和 setter、以及函数
    - 计算属性通常无需进行特殊处理

---
+++ meta/src/main/kotlin/net/yan100/compose/meta/annotations/orm/MetaFormula.kt
@@ -0,0 +1,19 @@
+package net.yan100.compose.meta.annotations.orm
+
+import java.lang.annotation.Inherited
+
+/**
+ * ## 计算属性
+ *
+ * 表示该元素为计算属性，大多情况下，无需进行处理
+ */
+@Target(
+  AnnotationTarget.PROPERTY,
+  AnnotationTarget.PROPERTY_GETTER,
+  AnnotationTarget.PROPERTY_SETTER,
+  AnnotationTarget.FUNCTION
+)
+@Inherited
+@MustBeDocumented
+@Retention(AnnotationRetention.BINARY)
+annotation class MetaFormula()
