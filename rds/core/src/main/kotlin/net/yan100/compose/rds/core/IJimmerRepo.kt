dc3c6b65532fafe45833163cc8cb6b9c3a7fc943
赵日天 <truenine304520@gmail.com>
2024-12-15 04:22:17 +0800

    refactor(rds-core): 优化 findByIdOrNull 方法并升级版本
    - 将 findByIdOrNull 方法中的 getOrNull() 替换为 orElse(null)，以提高代码可读性- 升级 jimmer 版本至 0.9.32
    - 更新项目版本号：  - composeVersionCatalog 从2.0.106到 2.0.107
      - composeRdsCrud 从 2.0.46 到 2.0.47
      - composeRdsCore 从 2.0.45 到 2.0.46
      - composeCore 保持不变
      - composeKspPlugin 保持不变
      - composeDependServlet 保持不变

--- rds/core/src/main/kotlin/net/yan100/compose/rds/core/IJimmerRepo.kt
+++ rds/core/src/main/kotlin/net/yan100/compose/rds/core/IJimmerRepo.kt
@@ -3,9 +3,8 @@ package net.yan100.compose.rds.core
 import net.yan100.compose.rds.core.entities.IJimmerPersistentEntity
 import org.babyfish.jimmer.spring.repository.KRepository
 import org.springframework.data.repository.NoRepositoryBean
-import kotlin.jvm.optionals.getOrNull
 
 @NoRepositoryBean
 interface IJimmerRepo<E : IJimmerPersistentEntity, ID : Any> : KRepository<E, ID> {
-  fun IJimmerRepo<E, ID>.findByIdOrNull(id: ID): E? = findById(id).getOrNull()
+  fun IJimmerRepo<E, ID>.findByIdOrNull(id: ID): E? = findById(id).orElse(null)
 }
