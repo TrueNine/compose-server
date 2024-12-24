d27f86b17406ceddd0aa13fb5d869ef6da2cf2da
赵日天 <truenine304520@gmail.com>
2024-12-24 17:41:53 +0800

    refactor(client): 重构 ClientTypeFns 中的类型转换函数- 移除了未使用的导入- 简化了 ClientType.toTypescriptEnum() 函数的实现 - 使用 TypescriptScope.Enum 替代 TypescriptEnum - 使用 TypescriptName.Name 封装类型名称

--- client/src/main/kotlin/net/yan100/compose/client/ClientTypeFns.kt
+++ client/src/main/kotlin/net/yan100/compose/client/ClientTypeFns.kt
@@ -1,21 +1,17 @@
 package net.yan100.compose.client
 
-import net.yan100.compose.client.domain.TypescriptEnum
-import net.yan100.compose.core.typing.StringTyping
+import net.yan100.compose.client.domain.TypescriptScope
+import net.yan100.compose.client.domain.entries.TypescriptName
 import net.yan100.compose.meta.client.ClientType
-import net.yan100.compose.meta.types.TypeKind
 
 internal fun String.typescriptUseName(): String {
   return this.split(".").last().split("$").last()
 }
 
 
-internal fun ClientType.toTypescriptEnum(): TypescriptEnum {
-  val isString = superTypes.any { it.typeKind == TypeKind.INTERFACE && it.typeName == StringTyping::class.qualifiedName }
-  return TypescriptEnum(
-    name = typeName.typescriptUseName(),
-    isExport = true,
-    isString = isString,
+internal fun ClientType.toTypescriptEnum(): TypescriptScope.Enum {
+  return TypescriptScope.Enum(
+    name = TypescriptName.Name(typeName.typescriptUseName()),
     constants = enumConstants
   )
 }
