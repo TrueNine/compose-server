9c317d96b3ecbaacfc7ec26d5dcefcaef1be3bc3
赵日天 <truenine304520@gmail.com>
2024-12-26 21:12:44 +0800

    refactor(client): 重构类型处理逻辑以支持泛型
    -重命名和重构了多个类型处理函数，以更好地支持泛型类型
    - 添加了新的函数来处理泛型类型的转换
    - 优化了类型名称和路径的处理逻辑，使其更适合 TypeScript 风格

--- client/src/main/kotlin/net/yan100/compose/client/ClientTypeFns.kt
+++ client/src/main/kotlin/net/yan100/compose/client/ClientTypeFns.kt
@@ -1,17 +1,62 @@
 package net.yan100.compose.client
 
-import net.yan100.compose.client.domain.TypescriptScope
-import net.yan100.compose.client.domain.entries.TypescriptName
+import net.yan100.compose.client.domain.TsGeneric
+import net.yan100.compose.client.domain.TsScope
+import net.yan100.compose.client.domain.TsTypeVal
+import net.yan100.compose.client.domain.entries.TsName
+import net.yan100.compose.meta.client.ClientInputGenericType
 import net.yan100.compose.meta.client.ClientType
 
-internal fun String.typescriptUseName(): String {
-  return this.split(".").last().split("$").last()
+internal fun String.toTsStyleName(): TsName.Name {
+  return TsName.Name(this.split(".").last().replace("$", "_"))
 }
 
+internal fun String.toTsStylePathName(): TsName.PathName {
+  val packageAndClassName = split(".")
+  if (packageAndClassName.size == 1) {
+    return TsName.PathName(
+      name = this.toTsStyleName().name,
+      path = ""
+    )
+  }
+  return TsName.PathName(
+    name = packageAndClassName.last().toTsStyleName().name,
+    path = packageAndClassName.dropLast(1).joinToString("_")
+  )
+}
 
-internal fun ClientType.toTypescriptEnum(): TypescriptScope.Enum {
-  return TypescriptScope.Enum(
-    name = TypescriptName.Name(typeName.typescriptUseName()),
-    constants = enumConstants
+internal fun ClientInputGenericType.toTsGenericUsed(
+  typeNameProvider: (ClientInputGenericType) -> TsName
+): List<TsGeneric.Used> {
+  return inputGenerics.map {
+    TsGeneric.Used(
+      used = TsTypeVal.TypeDef(
+        typeName = typeNameProvider(it),
+        usedGenerics = it.toTsGenericUsed(typeNameProvider)
+      ),
+      index = it.index
+    )
+  }
+}
+
+internal fun ClientType.toTsGenericUsed(
+  typeNameProvider: (ClientInputGenericType) -> TsName
+): List<TsGeneric.Used> {
+  return usedGenerics.mapIndexed { index, used ->
+    TsGeneric.Used(
+      used = TsTypeVal.TypeDef(
+        typeName = typeNameProvider(used),
+        usedGenerics = used.toTsGenericUsed(typeNameProvider)
+      ),
+      index = index
+    )
+  }
+}
+
+internal fun ClientType.toTypescriptEnum(): TsScope.Enum {
+  return TsScope.Enum(
+    meta = this,
+    name = typeName.toTsStylePathName(),
+    constants = resolveEnumConstants()
   )
 }
