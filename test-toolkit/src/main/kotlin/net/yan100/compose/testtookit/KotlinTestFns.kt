7701141ee0d016fd35a76e44589061d9878c3663
赵日天 <truenine304520@gmail.com>
2024-12-26 21:01:48 +0800

    test(test-toolkit): 优化集合和字符串断言函数
    -重写 assertNotEmpty 和 assertEmpty函数，使用更直观的断言方式
    - 新增 assertNotBlank 和 assertBlank 函数，用于字符串空白性断言

--- test-toolkit/src/main/kotlin/net/yan100/compose/testtookit/KotlinTestFns.kt
+++ test-toolkit/src/main/kotlin/net/yan100/compose/testtookit/KotlinTestFns.kt
@@ -1,11 +1,21 @@
 package net.yan100.compose.testtookit
 
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 import kotlin.test.assertTrue
 
 fun <T> assertNotEmpty(message: String? = "collection is empty", actual: () -> List<T>) {
-  assertTrue(message) { actual().isNotEmpty() }
+  assertNotEquals(0, actual().size, message)
 }
 
 fun <T> assertEmpty(message: String? = "collection is not empty", actual: () -> List<T>) {
-  assertTrue(message) { actual().isEmpty() }
+  assertEquals(0, actual().size, message)
+}
+
+fun assertNotBlank(string: String, message: String? = "string is blank") {
+  assertTrue(string.isNotBlank(), message)
+}
+
+fun assertBlank(string: String, message: String? = "string is not blank") {
+  assertTrue(string.isBlank(), message)
 }
