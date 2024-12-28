0ea69fe2c998c023cf88c7e93ab57b5cf06ee276
赵日天 <truenine304520@gmail.com>
2024-12-29 06:00:29 +0800

    feat(client): 添加 Map 类型的 TypeScript 代码转换支持
    - 新增 TsMapEntryInterceptor 和 TsMapPostInterceptor 两个拦截器
    - 支持 Java 和 Kotlin 的 Map 接口转换为 TypeScript 类型
    - 处理 Map.Entry 类型的转换

---
+++ client/src/main/kotlin/net/yan100/compose/client/interceptors/ts/TsMapEntryInterceptor.kt
@@ -0,0 +1,32 @@
+package net.yan100.compose.client.interceptors.ts
+
+import net.yan100.compose.client.contexts.KtToTsContext
+import net.yan100.compose.client.domain.TsGeneric
+import net.yan100.compose.client.domain.TsTypeProperty
+import net.yan100.compose.client.domain.TsTypeVal
+import net.yan100.compose.client.interceptors.TsPostReferenceInterceptor
+import net.yan100.compose.client.toTsStyleName
+import net.yan100.compose.meta.client.ClientType
+
+class TsMapEntryInterceptor : TsPostReferenceInterceptor() {
+  private val nameMap = listOf(
+    "java.util.Map\$Entry",
+    "kotlin.collections.Map\$Entry",
+  ).associateWith {
+    TsTypeVal.Object(
+      elements = listOf(
+        TsTypeProperty(
+          name = "key".toTsStyleName(),
+          defined = TsTypeVal.Generic(TsGeneric.UnUsed)
+        ),
+        TsTypeProperty(
+          name = "value".toTsStyleName(),
+          defined = TsTypeVal.Generic(TsGeneric.UnUsed)
+        )
+      )
+    )
+  }
+
+  override fun supported(ctx: KtToTsContext, source: ClientType): Boolean = source.typeName in nameMap
+  override fun process(ctx: KtToTsContext, source: ClientType): TsTypeVal = nameMap[source.typeName]!!
+}
---
+++ client/src/main/kotlin/net/yan100/compose/client/interceptors/ts/TsMapPostInterceptor.kt
@@ -0,0 +1,22 @@
+package net.yan100.compose.client.interceptors.ts
+
+import net.yan100.compose.client.contexts.KtToTsContext
+import net.yan100.compose.client.domain.TsGeneric
+import net.yan100.compose.client.domain.TsTypeVal
+import net.yan100.compose.client.interceptors.TsPostReferenceInterceptor
+import net.yan100.compose.meta.client.ClientType
+
+class TsMapPostInterceptor : TsPostReferenceInterceptor() {
+  private val nameMap = listOf(
+    "kotlin.collections.Map",
+    "java.util.Map"
+  ).associateWith {
+    TsTypeVal.Record(
+      keyUsedGeneric = TsGeneric.UnUsed,
+      valueUsedGeneric = TsGeneric.UnUsed
+    )
+  }
+
+  override fun supported(ctx: KtToTsContext, source: ClientType): Boolean = source.typeName in nameMap
+  override fun process(ctx: KtToTsContext, source: ClientType): TsTypeVal = nameMap[source.typeName]!!
+}
