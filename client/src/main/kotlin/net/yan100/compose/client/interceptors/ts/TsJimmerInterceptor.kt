9e3ea80fabfe40dd72bddfbff32711b79f0b6917
赵日天 <truenine304520@gmail.com>
2024-12-29 03:09:35 +0800

    refactor(client): 将 TsJimmerInterceptor 继承的接口从 KotlinToTypescriptInterceptor 改为 TsScopeInterceptor
    - 修改了 TsJimmerInterceptor 类的父类，以更好地适应项目结构和需求- 这个改动有助于提高代码的可维护性和扩展性

--- client/src/main/kotlin/net/yan100/compose/client/interceptors/ts/TsJimmerInterceptor.kt
+++ client/src/main/kotlin/net/yan100/compose/client/interceptors/ts/TsJimmerInterceptor.kt
@@ -4,11 +4,11 @@ import net.yan100.compose.client.contexts.ExecuteStage
 import net.yan100.compose.client.contexts.KtToTsContext
 import net.yan100.compose.client.domain.TsScope
 import net.yan100.compose.client.domain.TsTypeVal
-import net.yan100.compose.client.interceptors.KotlinToTypescriptInterceptor
+import net.yan100.compose.client.interceptors.TsScopeInterceptor
 import net.yan100.compose.meta.client.ClientType
 import net.yan100.compose.meta.types.TypeKind
 
-class TsJimmerInterceptor : KotlinToTypescriptInterceptor() {
+class TsJimmerInterceptor : TsScopeInterceptor() {
   override val executeStage: ExecuteStage = ExecuteStage.LOOP_RESOLVE_TS_REFERENCES
 
   private val jimmerTypeKinds = setOf(
