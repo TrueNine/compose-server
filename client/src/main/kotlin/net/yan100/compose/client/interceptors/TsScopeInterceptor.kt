98d28c3f80a7e906dd24390d778293cb2ed6b851
赵日天 <truenine304520@gmail.com>
2024-12-29 03:02:04 +0800

    refactor(client): 重命名 KotlinToTypescriptInterceptor 为 TsScopeInterceptor
    - 将抽象类名从 KotlinToTypescriptInterceptor 改为 TsScopeInterceptor
    - 更新文件名以匹配新的类名
    - 将默认处理方法的返回值从 TsTypeVal.Unknown 改为 TsTypeVal.Never

--- client/src/main/kotlin/net/yan100/compose/client/interceptors/TsScopeInterceptor.kt
+++ client/src/main/kotlin/net/yan100/compose/client/interceptors/TsScopeInterceptor.kt
@@ -9,11 +9,11 @@ import net.yan100.compose.meta.client.ClientType
 /**
  * 将 Kotlin 类型描述转换为 Typescript 类型
  */
-abstract class KotlinToTypescriptInterceptor : Interceptor<ClientType, TsScope, KtToTsContext> {
+abstract class TsScopeInterceptor : Interceptor<ClientType, TsScope, KtToTsContext> {
   override val executeStage: ExecuteStage = ExecuteStage.RESOLVE_TS_SCOPE
   override fun supported(ctx: KtToTsContext, source: ClientType) = false
   override fun defaultProcess(ctx: KtToTsContext, source: ClientType) = TsScope.TypeVal(
     meta = source,
-    definition = TsTypeVal.Unknown
+    definition = TsTypeVal.Never
   )
 }
