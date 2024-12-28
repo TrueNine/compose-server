b19b60d58e5a7086e041970862777dbfe592020d
赵日天 <truenine304520@gmail.com>
2024-12-29 03:05:40 +0800

    refactor(client): 重构 TsBuiltinInterceptor 类
    - 将 TsBuiltinInterceptor 类的父类从 KotlinToTypescriptInterceptor 改为 TsScopeInterceptor
    - 更新 import语句，引入新的 TsScopeInterceptor 类
    - 修改 executeStage 属性的值为 ExecuteStage.RESOLVE_TS_SCOPE

--- client/src/main/kotlin/net/yan100/compose/client/interceptors/ts/TsBuiltinInterceptor.kt
+++ client/src/main/kotlin/net/yan100/compose/client/interceptors/ts/TsBuiltinInterceptor.kt
@@ -3,10 +3,10 @@ package net.yan100.compose.client.interceptors.ts
 import net.yan100.compose.client.contexts.ExecuteStage
 import net.yan100.compose.client.contexts.KtToTsContext
 import net.yan100.compose.client.domain.TsScope
-import net.yan100.compose.client.interceptors.KotlinToTypescriptInterceptor
+import net.yan100.compose.client.interceptors.TsScopeInterceptor
 import net.yan100.compose.meta.client.ClientType
 
-class TsBuiltinInterceptor : KotlinToTypescriptInterceptor() {
+class TsBuiltinInterceptor : TsScopeInterceptor() {
   override val executeStage: ExecuteStage = ExecuteStage.RESOLVE_TS_SCOPE
   private val nameTable = mapOf<String, TsScope>()
 
