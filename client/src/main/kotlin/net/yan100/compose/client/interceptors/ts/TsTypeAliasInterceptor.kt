2ad02a00cee796cd0b396118e47473fe1c72ecd4
赵日天 <truenine304520@gmail.com>
2024-12-29 03:08:52 +0800

    refactor(client): 重构类型别名拦截器
    - 将 TsTypeAliasInterceptor 从 KotlinToTypescriptInterceptor 继承改为 TsScopeInterceptor 继承
    - 修改 supported 和 process 方法以适应新的拦截器类型
    - 优化 generics 的处理逻辑，使用 arguments 替代 argumentLocations

--- client/src/main/kotlin/net/yan100/compose/client/interceptors/ts/TsTypeAliasInterceptor.kt
+++ client/src/main/kotlin/net/yan100/compose/client/interceptors/ts/TsTypeAliasInterceptor.kt
@@ -5,17 +5,17 @@ import net.yan100.compose.client.contexts.KtToTsContext
 import net.yan100.compose.client.domain.TsGeneric
 import net.yan100.compose.client.domain.TsScope
 import net.yan100.compose.client.domain.entries.TsName
-import net.yan100.compose.client.interceptors.KotlinToTypescriptInterceptor
+import net.yan100.compose.client.interceptors.TsScopeInterceptor
 import net.yan100.compose.client.toTsGenericUsed
 import net.yan100.compose.client.toTsStylePathName
 import net.yan100.compose.meta.client.ClientType
 import net.yan100.compose.meta.types.TypeKind
 
-class TsTypeAliasInterceptor : KotlinToTypescriptInterceptor() {
+class TsTypeAliasInterceptor : TsScopeInterceptor() {
   override val executeStage: ExecuteStage = ExecuteStage.RESOLVE_TS_SCOPE
   override fun supported(ctx: KtToTsContext, source: ClientType): Boolean = source.typeKind == TypeKind.TYPEALIAS
   override fun process(ctx: KtToTsContext, source: ClientType): TsScope {
-    val generics = source.argumentLocations.mapIndexed { i, it ->
+    val generics = source.arguments.mapIndexed { i, it ->
       TsGeneric.Defined(
         name = TsName.Name(it),
         index = i
