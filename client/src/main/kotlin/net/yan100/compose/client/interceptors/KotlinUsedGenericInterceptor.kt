5518aa026d942c18b819f0c35d2d75919625947c
赵日天 <truenine304520@gmail.com>
2024-12-29 02:29:46 +0800

    refactor(client): 更新 KotlinUsedGenericInterceptor 类型参数
    - 将 KotlinUsedGenericInterceptor 类的泛型参数从 ClientInputGenericType 改为 ClientUsedGeneric
    - 更新相关函数的参数和返回类型，以适应新的泛型参数

--- client/src/main/kotlin/net/yan100/compose/client/interceptors/KotlinUsedGenericInterceptor.kt
+++ client/src/main/kotlin/net/yan100/compose/client/interceptors/KotlinUsedGenericInterceptor.kt
@@ -2,20 +2,20 @@ package net.yan100.compose.client.interceptors
 
 import net.yan100.compose.client.contexts.ExecuteStage
 import net.yan100.compose.client.contexts.KtToKtContext
-import net.yan100.compose.meta.client.ClientInputGenericType
+import net.yan100.compose.meta.client.ClientUsedGeneric
 
-class KotlinUsedGenericInterceptor : Interceptor<ClientInputGenericType, ClientInputGenericType, KtToKtContext> {
+class KotlinUsedGenericInterceptor : Interceptor<ClientUsedGeneric, ClientUsedGeneric, KtToKtContext> {
   override val executeStage: ExecuteStage = ExecuteStage.CONVERT_QUALIFIER_NAME
 
-  override fun supported(ctx: KtToKtContext, source: ClientInputGenericType): Boolean {
+  override fun supported(ctx: KtToKtContext, source: ClientUsedGeneric): Boolean {
     TODO("Not yet implemented")
   }
 
-  override fun process(ctx: KtToKtContext, source: ClientInputGenericType): ClientInputGenericType {
+  override fun process(ctx: KtToKtContext, source: ClientUsedGeneric): ClientUsedGeneric {
     TODO("Not yet implemented")
   }
 
-  override fun defaultProcess(ctx: KtToKtContext, source: ClientInputGenericType): ClientInputGenericType {
+  override fun defaultProcess(ctx: KtToKtContext, source: ClientUsedGeneric): ClientUsedGeneric {
     TODO("Not yet implemented")
   }
 }
