64ec7406e8fb70a5a90b66cc49329f1cc5fcabbb
赵日天 <truenine304520@gmail.com>
2024-12-29 05:59:39 +0800

    refactor(client): 重构列表类型拦截器
    - 移除了未使用的 ExecuteStage 引入
    - 添加了 nameMap，用于存储需要拦截的 Java 类型与对应 TsTypeVal 的映射关系
    - 重写了 supported 方法，支持拦截更多集合类型
    - 简化了 process 方法的实现

--- client/src/main/kotlin/net/yan100/compose/client/interceptors/ts/TsListTypeValInterceptor.kt
+++ client/src/main/kotlin/net/yan100/compose/client/interceptors/ts/TsListTypeValInterceptor.kt
@@ -1,6 +1,5 @@
 package net.yan100.compose.client.interceptors.ts
 
-import net.yan100.compose.client.contexts.ExecuteStage
 import net.yan100.compose.client.contexts.KtToTsContext
 import net.yan100.compose.client.domain.TsGeneric
 import net.yan100.compose.client.domain.TsTypeVal
@@ -8,18 +7,21 @@ import net.yan100.compose.client.interceptors.TsPostReferenceInterceptor
 import net.yan100.compose.meta.client.ClientType
 
 open class TsListTypeValInterceptor : TsPostReferenceInterceptor() {
-  override val executeStage: ExecuteStage = ExecuteStage.LOOP_RESOLVE_TS_REFERENCES
+  private val nameMap = listOf(
+    "java.util.Iterable",
+    "java.util.Collection",
+    "java.lang.List",
+    "java.util.Set"
+  ).associateWith { TsTypeVal.Array(usedGeneric = TsGeneric.UnUsed) }
 
   override fun supported(ctx: KtToTsContext, source: ClientType): Boolean {
-    return false
+    return nameMap.containsKey(source.typeName)
+      || nameMap.any { source.isAssignableFrom(it.key) }
   }
 
   override fun process(ctx: KtToTsContext, source: ClientType): TsTypeVal {
     return TsTypeVal.Array(
-      TsGeneric.Used(
-        used = TsTypeVal.Never,
-        index = 0
-      )
+      TsGeneric.UnUsed
     )
   }
 }
