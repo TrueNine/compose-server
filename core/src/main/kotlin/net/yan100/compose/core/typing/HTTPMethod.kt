d015ec1edabfdb3839fad8ffe4d7b44c5b1307b7
赵日天 <truenine304520@gmail.com>
2024-12-31 21:05:28 +0800

    feat(core): 添加 HTTP 方法枚举类
    - 创建 HTTPMethod 枚举类，定义了常见的 HTTP 方法
    - 实现 StringTyping接口，提供统一的字符串表示
    - 添加伴生对象，实现根据方法名获取对应的枚举值

---
+++ core/src/main/kotlin/net/yan100/compose/core/typing/HTTPMethod.kt
@@ -0,0 +1,22 @@
+package net.yan100.compose.core.typing
+
+enum class HTTPMethod(
+  val methodName: String
+) : StringTyping {
+  GET("GET"),
+  POST("POST"),
+  PUT("PUT"),
+  DELETE("DELETE"),
+  PATCH("PATCH"),
+  HEAD("HEAD"),
+  OPTIONS("OPTIONS"),
+  TRACE("TRACE"),
+  CONNECT("CONNECT"),
+  ;
+
+  override val value: String = methodName
+
+  companion object {
+    operator fun get(methodName: String?): HTTPMethod? = entries.firstOrNull { it.methodName == methodName?.uppercase() }
+  }
+}
