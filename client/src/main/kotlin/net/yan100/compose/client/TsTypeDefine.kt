478cee62fa2841e0a87e1923b9639106dc928f20
赵日天 <truenine304520@gmail.com>
2025-01-01 15:09:32 +0800

    feat(client): 新增 TsTypeDefine 接口
    - 定义了用于描述 TypeScript 类型的接口
    - 包含判断是否需要填充泛型参数的方法
    - 提供了填充泛型参数的方法

---
+++ client/src/main/kotlin/net/yan100/compose/client/TsTypeDefine.kt
@@ -0,0 +1,20 @@
+package net.yan100.compose.client
+
+import net.yan100.compose.client.domain.TsGeneric
+
+interface TsTypeDefine<T : TsTypeDefine<T>> {
+  /**
+   * 判定其内包括自身属性是否具有 [net.yan100.compose.client.domain.TsGeneric.UnUsed] 标记
+   *
+   * 进而决定是否应该向其内部填充泛型参数
+   */
+  val isRequireUseGeneric: Boolean
+
+  /**
+   * 判定自身是否为基本 无引用类型，无需填充泛型
+   */
+  val isBasic: Boolean
+
+  fun fillGenerics(vararg generic: TsGeneric): T
+  fun fillGenerics(usedGenerics: List<TsGeneric>): T
+}
