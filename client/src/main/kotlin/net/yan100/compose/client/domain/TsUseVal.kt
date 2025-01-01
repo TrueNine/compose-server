c41e194a0982ef0d9ff07210564b1bb1d058e000
赵日天 <truenine304520@gmail.com>
2025-01-01 15:47:59 +0800

    feat(client): 为 TsUseVal 类增加泛型填充功能
    - 在 TsUseVal 类中实现 TsTypeDefine 接口
    - 添加 isBasic 和 isRequireUseGeneric属性
    - 实现 fillGenerics 方法以支持泛型填充
    - 优化 ParameterType 和 ReturnType 类

--- client/src/main/kotlin/net/yan100/compose/client/domain/TsUseVal.kt
+++ client/src/main/kotlin/net/yan100/compose/client/domain/TsUseVal.kt
@@ -1,11 +1,38 @@
 package net.yan100.compose.client.domain
 
+import net.yan100.compose.client.TsTypeDefine
 import net.yan100.compose.client.domain.entries.TsName
 
 sealed class TsUseVal<T : TsUseVal<T>>(
   open val typeValue: TsTypeVal<*>,
   open val partial: Boolean = false
-) {
+) : TsTypeDefine<T> {
+  override val isBasic: Boolean
+    get() = when (this) {
+      is ParameterType -> typeValue.isBasic
+      is ReturnType -> typeValue.isBasic
+    }
+  override val isRequireUseGeneric: Boolean
+    get() = when (this) {
+      is ParameterType -> typeValue.isRequireUseGeneric
+      is ReturnType -> typeValue.isRequireUseGeneric
+    }
+
+  @Suppress("UNCHECKED_CAST")
+  override fun fillGenerics(vararg generic: TsGeneric): T {
+    if (generic.isEmpty()) return this as T
+    return fillGenerics(generic.toList())
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  override fun fillGenerics(usedGenerics: List<TsGeneric>): T {
+    if (usedGenerics.isEmpty()) return this as T
+    return when (this) {
+      is ParameterType -> copy(typeValue = typeValue.fillGenerics(usedGenerics)) as T
+      is ReturnType -> copy(typeValue = typeValue.fillGenerics(usedGenerics)) as T
+    }
+  }
+
   data class ReturnType(
     override val typeValue: TsTypeVal<*>,
     override val partial: Boolean = false
