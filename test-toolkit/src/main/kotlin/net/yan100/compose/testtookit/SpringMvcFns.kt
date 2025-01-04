66fe729f173000af6689cde4c6b427af9a942eb8
赵日天 <truenine304520@gmail.com>
2025-01-05 02:16:10 +0800

    feat(test-toolkit): 添加 MockMultipartFile 复制函数
    - 在 test-toolkit 模块中添加 SpringMvcFns.kt 文件- 实现 MockMultipartFile 的 copy 函数，支持创建副本并可修改部分属性

---
+++ test-toolkit/src/main/kotlin/net/yan100/compose/testtookit/SpringMvcFns.kt
@@ -0,0 +1,17 @@
+package net.yan100.compose.testtookit
+
+import org.springframework.mock.web.MockMultipartFile
+
+
+fun MockMultipartFile.copy(
+  name: String = this.name,
+  originalFilename: String = this.originalFilename,
+  contentType: String? = this.contentType,
+  content: ByteArray = this.inputStream.readAllBytes()
+): MockMultipartFile = MockMultipartFile(
+  name,
+  originalFilename,
+  contentType,
+  content
+)
+
