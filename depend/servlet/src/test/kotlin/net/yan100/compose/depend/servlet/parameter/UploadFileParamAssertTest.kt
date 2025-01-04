c84b5e2fa7fdde8a65cee46dd418cae9350f3e8b
赵日天 <truenine304520@gmail.com>
2025-01-05 02:14:19 +0800

    test(servlet): 添加文件上传相关测试
    - 新增 TestUploadController 类，包含多个文件上传测试接口
    - 新增 UploadFileParamAssertTest 类，实现文件上传的单元测试
    - 测试内容包括上传文件列表、混合 DTO 上传、上传 JSON 表单及文件等

---
+++ depend/servlet/src/test/kotlin/net/yan100/compose/depend/servlet/parameter/UploadFileParamAssertTest.kt
@@ -0,0 +1,65 @@
+package net.yan100.compose.depend.servlet.parameter
+
+import jakarta.annotation.Resource
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.mock.web.MockMultipartFile
+import org.springframework.mock.web.MockPart
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.multipart
+import java.nio.charset.StandardCharsets
+import kotlin.test.Test
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class UploadFileParamAssertTest {
+  lateinit var mvc: MockMvc @Resource set
+  val json = """{"a":"str","b": 1023}"""
+  val jsonFile = MockMultipartFile(
+    "json",
+    "json",
+    "application/json",
+    json.toByteArray(StandardCharsets.UTF_8)
+  )
+  val file = MockMultipartFile(
+    "file",
+    "file",
+    "text/plain",
+    "Hello, World!".toByteArray()
+  )
+
+  @Test
+  fun `测试 上传文件列表`() {
+    mvc.multipart(HttpMethod.POST, "/testUploadController/uploadFileList") {
+      param("json.a", "str")
+      param("json.b", "1")
+      part(MockPart("file", "", "Hello, World!".toByteArray(), MediaType.TEXT_PLAIN))
+    }.andExpect {
+      status { isOk() }
+    }
+  }
+
+  @Test
+  fun `测试 混合 dto 上传`() {
+    mvc.multipart(HttpMethod.POST, "/testUploadController/uploadBlend") {
+      param("json.a", "str")
+      param("json.b", "1")
+      part(MockPart("file", "", "Hello, World!".toByteArray(), MediaType.TEXT_PLAIN))
+    }.andExpect {
+      status { isOk() }
+    }
+  }
+
+  @Test
+  fun `测试 上传 json 表单以及 文件`() {
+    mvc.multipart(HttpMethod.POST, "/testUploadController/uploadFileAndOtherField") {
+      contentType = MediaType.MULTIPART_FORM_DATA
+      file(jsonFile)
+      file(file)
+    }.andExpect {
+      status { isOk() }
+    }
+  }
+}
