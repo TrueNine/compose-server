07fe33dc439c2a95121e27ff671a0b51b6b83cd2
赵日天 <truenine304520@gmail.com>
2025-01-06 18:57:32 +0800

    feat(client): 增加作用域符号枚举
    - 新增 ARROW、ASSIGN、ASSIGNMENT 三个枚举值
    - 增加 ASSIGN_OBJECT 枚举值，用于对象赋值- 让 TsScopeQuota继承自 TsSymbol 接口

--- client/src/main/kotlin/net/yan100/compose/client/domain/TsScopeQuota.kt
+++ client/src/main/kotlin/net/yan100/compose/client/domain/TsScopeQuota.kt
@@ -1,13 +1,19 @@
 package net.yan100.compose.client.domain
 
+import net.yan100.compose.client.TsSymbol
+
 /**
  * 作用域符号枚举
  */
 enum class TsScopeQuota(
   val left: String,
   val right: String
-) {
+) : TsSymbol {
+  ARROW("=>", ""),
+  ASSIGN("=", ""),
+  ASSIGNMENT(":", ""),
   OBJECT("{", "}"),
+  ASSIGN_OBJECT("= {", "}"),
   ARRAY("[", "]"),
   BRACKETS("(", ")"),
   BLANK("", "");
