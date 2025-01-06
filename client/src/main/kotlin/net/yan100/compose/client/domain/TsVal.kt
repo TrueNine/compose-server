27f6943f3f5160494f664b7c1c2a3b629c1d6137
赵日天 <truenine304520@gmail.com>
2025-01-06 19:01:01 +0800

    feat(client): 添加函数代码生成能力
    - 在 Function 类中添加 CodeRender 和 CodeBuildable 接口的实现
    - 新增 code属性，用于生成函数的 TypeScript代码
    - 为 Function 类添加 parentScope 参数，用于指定父作用域

--- client/src/main/kotlin/net/yan100/compose/client/domain/TsVal.kt
+++ client/src/main/kotlin/net/yan100/compose/client/domain/TsVal.kt
@@ -1,5 +1,7 @@
 package net.yan100.compose.client.domain
 
+import net.yan100.compose.client.CodeBuildable
+import net.yan100.compose.client.CodeRender
 import net.yan100.compose.client.domain.entries.TsName
 
 sealed class TsVal(
@@ -13,12 +15,20 @@ sealed class TsVal(
   ) : TsVal(modifiers = modifiers, quota = quota)
 
   data class Function(
-    override val modifiers: List<TsModifier> = emptyList(),
     val name: TsName.Name,
+    val parentScope: TsScope.Class? = null,
     val params: List<TsUseVal.Parameter> = emptyList(),
     val returnType: TsUseVal.Return = TsUseVal.Return(TsTypeVal.Void),
+    override val modifiers: List<TsModifier> = emptyList(),
     override val quota: TsScopeQuota = TsScopeQuota.OBJECT,
+    private val render: CodeRender<Function> = {},
   ) : TsVal(modifiers = modifiers, quota = quota) {
+    val code: String
+      get() {
+        val cb = CodeBuildable<Function>()
+        this.render(cb, this)
+        return cb.toString()
+      }
     val isAsync: Boolean = returnType.typeVal is TsTypeVal.Promise
   }
 
