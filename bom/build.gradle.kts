cfe933a0ff3c1e4018ecd547e20f166a2f01d9eb
赵日天 <truenine304520@gmail.com>
2025-01-07 05:30:53 +0800

    build(bom): 新增 java-platform 插件并更新依赖约束
    - 添加 java-platform 插件以管理平台依赖
    - 引入 publish-convention 插件
    - 设置版本为 composeVersionCatalog
    - 更新多个子项目的依赖约束

---
+++ bom/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+  `java-platform`
+  `publish-convention`
+}
+
+version = libs.versions.composeVersionCatalog.get()
+
+dependencies {
+  constraints {
+    //api(projects.gradlePlugin)
+    api(projects.cacheable)
+    api(projects.core)
+    api(projects.client)
+    api(projects.data.dataExtract)
+    api(projects.data.dataCrawler)
+    api(projects.depend.dependHttpExchange)
+    api(projects.depend.dependJackson)
+    api(projects.depend.dependJsr303Validation)
+    api(projects.depend.dependPaho)
+    api(projects.depend.dependServlet)
+    api(projects.depend.dependSpringdocOpenapi)
+    api(projects.depend.dependXxlJob)
+
+    api(projects.ksp.kspClient)
+    api(projects.ksp.kspPlugin)
+    api(projects.ksp.kspToolkit)
+    api(projects.meta)
+    api(projects.oss)
+    api(projects.pay)
+    api(projects.rds.rdsCore)
+    api(projects.rds.rdsCrud)
+    api(projects.rds.rdsMigrationH2)
+    api(projects.rds.rdsMigrationMysql)
+    api(projects.rds.rdsMigrationPostgres)
+    api(projects.security.securityCrypto)
+    api(projects.security.securityOauth2)
+    api(projects.security.securitySpring)
+    api(projects.testToolkit)
+    api(projects.versionCatalog)
+  }
+}
