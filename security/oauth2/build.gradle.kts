1477f9ccc3e52bc51c31b95be52710b39c82fab1
赵日天 <truenine304520@gmail.com>
2025-01-07 05:21:02 +0800

    build(oauth2): 更新构建配置并移除冗余设置
    - 添加 `kotlinspring-convention` 插件
    - 更新依赖项引用格式
    - 移除 publishing 和 signing 配置- 删除 testImplementation(project(":test-toolkit")) 重复引用

--- security/oauth2/build.gradle.kts
+++ security/oauth2/build.gradle.kts
@@ -1,34 +1,19 @@
+plugins {
+  `kotlinspring-convention`
+}
+
 project.version = libs.versions.composeSecurityOauth2.get()
 
 dependencies {
-  implementation(libs.org.springframework.boot.springBootAutoconfigure)
-  kapt(libs.org.springframework.springBootConfigurationProcessor)
-
   implementation(libs.org.jetbrains.kotlinx.kotlinxCoroutinesCore)
 
-  implementation(project(":core"))
-  implementation(project(":depend:depend-http-exchange"))
+  implementation(projects.core)
+  implementation(projects.depend.dependHttpExchange)
   implementation(libs.org.springframework.boot.springBootStarterWebflux)
 
-  implementation(project(":security:security-crypto"))
+  implementation(projects.security.securityCrypto)
   implementation(libs.org.springframework.security.springSecurityCore)
 
   testImplementation(libs.org.springframework.boot.springBootStarterWeb)
-  testImplementation(project(":test-toolkit"))
-}
-
-publishing {
-  publications {
-    create<MavenPublication>("maven") {
-      groupId = project.group.toString()
-      artifactId = project.name
-      version = project.version.toString()
-      from(components["java"])
-    }
-  }
-}
-
-signing {
-  useGpgCmd()
-  sign(publishing.publications["maven"])
+  testImplementation(projects.testToolkit)
 }
