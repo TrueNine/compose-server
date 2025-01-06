2bd09a0412a0f21aef502393c8687f992c6a497b
赵日天 <truenine304520@gmail.com>
2025-01-07 05:32:09 +0800

    build(cacheable): 更新构建配置并移除冗余代码
    - 添加 kotlinspring-convention 插件
    - 移除 publishing 和 signing 配置
    - 使用 projects.* 替代 project(":*") 引用其他模块

--- cacheable/build.gradle.kts
+++ cacheable/build.gradle.kts
@@ -1,28 +1,14 @@
+plugins {
+  `kotlinspring-convention`
+}
+
 version = libs.versions.composeCacheable.get()
 
 dependencies {
-
   implementation(libs.bundles.redis)
   implementation(libs.com.github.benManes.caffeine.caffeine)
 
-  implementation(project(":depend:depend-jackson"))
-  implementation(project(":core"))
-
-  testImplementation(project(":test-toolkit"))
-}
-
-publishing {
-  publications {
-    create<MavenPublication>("maven") {
-      groupId = project.group.toString()
-      artifactId = project.name
-      version = project.version.toString()
-      from(components["java"])
-    }
-  }
-}
-
-signing {
-  useGpgCmd()
-  sign(publishing.publications["maven"])
+  implementation(projects.depend.dependJackson)
+  implementation(projects.core)
+  testImplementation(projects.testToolkit)
 }
