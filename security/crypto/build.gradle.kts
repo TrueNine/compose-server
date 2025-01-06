3a813069b4ff6ceea663ba61b649b4e54f27e49b
赵日天 <truenine304520@gmail.com>
2025-01-07 05:22:19 +0800

    build(crypto): 更新项目配置和依赖
    - 添加 `kotlinspring-convention` 插件
    - 更新 dependencies 块格式
    - 移除 publishing 和 signing 配置
    - 更新项目依赖引用格式

--- security/crypto/build.gradle.kts
+++ security/crypto/build.gradle.kts
@@ -1,30 +1,15 @@
+plugins {
+  `kotlinspring-convention`
+}
+
 version = libs.versions.composeSecurityCrypto.get()
 
 dependencies {
-  implementation(libs.org.springframework.boot.springBootAutoconfigure)
-  kapt(libs.org.springframework.springBootConfigurationProcessor)
-
   api(libs.org.springframework.security.springSecurityCrypto)
 
   implementation(libs.org.bouncycastle.bcprovJdk18on)
-  implementation(project(":core"))
+  implementation(projects.core)
 
   testImplementation(libs.com.fasterxml.jackson.core.jacksonDatabind)
-  testImplementation(project(":test-toolkit"))
-}
-
-publishing {
-  publications {
-    create<MavenPublication>("maven") {
-      groupId = project.group.toString()
-      artifactId = project.name
-      version = project.version.toString()
-      from(components["java"])
-    }
-  }
-}
-
-signing {
-  useGpgCmd()
-  sign(publishing.publications["maven"])
+  testImplementation(projects.testToolkit)
 }
