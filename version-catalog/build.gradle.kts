f68c037a23cde29a4b9d6b5909233c0cf5e329f2
赵日天 <truenine304520@gmail.com>
2025-01-07 05:14:00 +0800

    build(version-catalog): 移除版本目录的发布配置
    - 删除了 publishing 和 signing 配置块
    - 移除了 version-catalog 特定的插件应用- 保留了 version-catalog 的基本配置

--- version-catalog/build.gradle.kts
+++ version-catalog/build.gradle.kts
@@ -1,41 +1,8 @@
-val yunxiaoUrl by extra { properties["url.yunxiao.1"] as String }
-val yunxiaoUsername by extra { properties["usr.yunxiao.1"] as String }
-val yunxiaoPassword by extra { properties["pwd.yunxiao.1"] as String }
-
 plugins {
-  signing
   `version-catalog`
-  `maven-publish`
+  `publish-convention`
 }
 
-group = libs.versions.composeGroup.get()
 version = libs.versions.composeVersionCatalog.get()
-
-publishing {
-  repositories {
-    mavenLocal()
-    maven(url = uri(yunxiaoUrl)) {
-      isAllowInsecureProtocol = false
-      credentials {
-        username = yunxiaoUsername
-        password = yunxiaoPassword
-      }
-    }
-  }
-
-  publications {
-    create<MavenPublication>("versionCatalog") {
-      groupId = project.group.toString()
-      artifactId = project.name
-      version = project.version.toString()
-      from(components["versionCatalog"])
-    }
-  }
-}
-
-signing {
-  useGpgCmd()
-  sign(publishing.publications["versionCatalog"])
-}
 
 catalog { versionCatalog { from(files("../gradle/libs.versions.toml")) } }
