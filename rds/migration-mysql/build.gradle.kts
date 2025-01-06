3f58abd7a6497399d1b86ff37082410be1c931f6
赵日天 <truenine304520@gmail.com>
2025-01-07 05:28:11 +0800

    build(rds): 移除 SQL 迁移模块的 Maven 发布配置
    - 从 H2、MySQL 和 PostgreSQL 迁移模块的 build.gradle.kts 文件中移除了 publishing 和 signing 配置
    - 添加了 `java-convention` 和 `sqlmigration-convention` 插件
    - 简化了版本定义方式

--- rds/migration-mysql/build.gradle.kts
+++ rds/migration-mysql/build.gradle.kts
@@ -1,17 +1,6 @@
+plugins {
+  `java-convention`
+  `sqlmigration-convention`
+}
+
 version = libs.versions.composeRdsMigrationMysql.get()
-
-publishing {
-  publications {
-    create<MavenPublication>("maven") {
-      groupId = project.group.toString()
-      artifactId = project.name
-      version = project.version.toString()
-      from(components["java"])
-    }
-  }
-}
-
-signing {
-  useGpgCmd()
-  sign(publishing.publications["maven"])
-}
