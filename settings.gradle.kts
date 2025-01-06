19c722835e065af3c6423bb0e25442de6027c608
赵日天 <truenine304520@gmail.com>
2025-01-06 20:33:29 +0800

    build: 移除设置.gradle.kts中的依赖解析管理配置
    - 删除了 settings.gradle.kts 文件中的 dependencyResolutionManagement 块
    - 这可能是为了简化项目配置或使用其他方式管理依赖版本

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,6 @@ plugins {
   id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-dependencyResolutionManagement { versionCatalogs { create("libs") { from(files("libs.versions.toml")) } } }
-
 rootProject.name = "compose-server"
 includeBuild("version-catalog")
 
