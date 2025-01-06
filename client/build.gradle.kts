9fb287492adccbba3227bb6d7ca90d5b123f48ae
赵日天 <truenine304520@gmail.com>
2025-01-07 05:42:57 +0800

    build(client): 更新构建配置并移除冗余设置
    - 移除版本定义，使用 composeClient版本
    - 添加 spring 配置项
    - 更新项目依赖引用方式
    - 移除 publishing 和 signing 配置

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,41 +1,25 @@
 plugins {
   alias(libs.plugins.com.google.devtools.ksp)
+  `kotlinspring-convention`
 }
 
 version = libs.versions.composeClient.get()
 
 dependencies {
-  implementation(libs.org.springframework.boot.springBootAutoconfigure)
-  kapt(libs.org.springframework.springBootConfigurationProcessor)
-
-  implementation(project(":meta"))
-  implementation(project(":core"))
+  implementation(projects.meta)
+  implementation(projects.core)
 
   implementation(libs.com.fasterxml.jackson.core.jacksonDatabind)
   implementation(libs.org.springframework.springWebMvc)
   implementation(libs.org.springframework.springWebflux)
 
   testImplementation(libs.org.springframework.boot.springBootStarterWeb)
-  kspTest(project(":ksp:ksp-client"))
+  kspTest(projects.ksp.kspClient)
+
+  testImplementation(projects.testToolkit)
+  testImplementation(projects.depend.dependJackson)
+
   kspTest(libs.org.babyfish.jimmer.jimmerKsp)
-  testImplementation(project(":test-toolkit"))
   testImplementation(libs.org.springframework.boot.springBootStarterWeb)
   testImplementation(libs.org.babyfish.jimmer.jimmerSpringBootStarter)
-  testImplementation(project(":depend:depend-jackson"))
-}
-
-publishing {
-  publications {
-    create<MavenPublication>("maven") {
-      groupId = project.group.toString()
-      artifactId = project.name
-      version = project.version.toString()
-      from(components["java"])
-    }
-  }
-}
-
-signing {
-  useGpgCmd()
-  sign(publishing.publications["maven"])
 }
