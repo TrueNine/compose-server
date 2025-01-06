b8f26185a84276999435ff5348d09028cb7d342a
赵日天 <truenine304520@gmail.com>
2025-01-07 05:42:06 +0800

    build(core): 更新构建配置并移除冗余代码
    - 添加 `kotlinspring-convention` 和 `publish-convention` 插件
    - 更新依赖项引用格式
    - 移除 publishing 和 signing 配置，减少冗余代码

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,12 +1,14 @@
+plugins {
+  `kotlinspring-convention`
+  `publish-convention`
+}
+
 version = libs.versions.composeCore.get()
 
 dependencies {
-  implementation(project(":meta"))
-  implementation(libs.org.springframework.boot.springBootAutoconfigure)
-  kapt(libs.org.springframework.springBootConfigurationProcessor)
+  implementation(projects.meta)
 
   api(libs.com.fasterxml.jackson.core.jacksonAnnotations)
-  //api(libs.jakarta.persistence.jakartaPersistenceApi)
   api(libs.jakarta.annotation.jakartaAnnotationApi)
   api(libs.jakarta.servlet.jakartaServletApi)
   api(libs.io.swagger.core.v3.swaggerAnnotationsJakarta)
@@ -14,22 +16,6 @@ dependencies {
 
   implementation(libs.org.springframework.security.springSecurityCrypto)
 
-  testImplementation(project(":test-toolkit"))
+  testImplementation(projects.testToolkit)
   testImplementation(libs.org.springframework.boot.springBootStarterWeb)
-}
-
-publishing {
-  publications {
-    create<MavenPublication>("maven") {
-      groupId = project.group.toString()
-      artifactId = project.name
-      version = project.version.toString()
-      from(components["java"])
-    }
-  }
-}
-
-signing {
-  useGpgCmd()
-  sign(publishing.publications["maven"])
 }
