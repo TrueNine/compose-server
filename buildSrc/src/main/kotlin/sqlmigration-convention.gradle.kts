3f6c073b0299e4eeeb38e83d42ea220256ebcac5
赵日天 <truenine304520@gmail.com>
2025-01-10 08:01:52 +0800

    style: 移除 SQL 文件的缩进配置
    移除了 spotless 插件中关于 SQL 文件缩进的配置，保持其他格式化配置不变。

--- buildSrc/src/main/kotlin/sqlmigration-convention.gradle.kts
+++ buildSrc/src/main/kotlin/sqlmigration-convention.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 // https://github.com/diffplug/spotless/tree/main/plugin-gradle#quickstart
 spotless {
   sql {
-    indentWithSpaces(2)
     lineEndings = LineEnding.UNIX
     target("**/**.sql")
     dbeaver().configFile(file(rootProject.layout.projectDirectory.file("buildSrc/.compose-config/.spotless_format_config.properties")))
