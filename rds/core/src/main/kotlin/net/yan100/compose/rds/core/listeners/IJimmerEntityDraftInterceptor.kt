28406165bcd09dc16ac18d7a8392d48efa9fcca5
赵日天 <truenine304520@gmail.com>
2025-01-11 10:05:19 +0800

    feat(core): 优化实体草稿拦截器
    - 添加 dependencies() 方法，指定需要加载的实体属性
    - 新增 beforeSaveAll() 方法，批量处理保存前的逻辑
    - 优化 beforeSave() 方法，仅在数据库元数据未加载时进行处理

--- rds/core/src/main/kotlin/net/yan100/compose/rds/core/listeners/IJimmerEntityDraftInterceptor.kt
+++ rds/core/src/main/kotlin/net/yan100/compose/rds/core/listeners/IJimmerEntityDraftInterceptor.kt
@@ -3,10 +3,25 @@ package net.yan100.compose.rds.core.listeners
 import net.yan100.compose.core.datetime
 import net.yan100.compose.rds.core.entities.IJimmerEntity
 import net.yan100.compose.rds.core.entities.IJimmerEntityDraft
+import net.yan100.compose.rds.core.entities.IJimmerEntityProps
 import org.babyfish.jimmer.kt.isLoaded
+import org.babyfish.jimmer.meta.TypedProp
 import org.babyfish.jimmer.sql.DraftInterceptor
 
 class IJimmerEntityDraftInterceptor : DraftInterceptor<IJimmerEntity, IJimmerEntityDraft> {
+  override fun dependencies(): Collection<TypedProp<IJimmerEntity, *>> {
+    return listOf(
+      IJimmerEntityProps.ID,
+      IJimmerEntityProps.DATABASE_METADATA
+    )
+  }
+
+  override fun beforeSaveAll(items: Collection<DraftInterceptor.Item<IJimmerEntity, IJimmerEntityDraft>>) {
+    items.forEach {
+      beforeSave(it.draft, it.original)
+    }
+  }
+
   override fun beforeSave(draft: IJimmerEntityDraft, original: IJimmerEntity?) {
     if (!isLoaded(draft, IJimmerEntity::databaseMetadata)) {
       if (original === null) {
@@ -16,11 +31,11 @@ class IJimmerEntityDraftInterceptor : DraftInterceptor<IJimmerEntity, IJimmerEnt
           rlv = 0
           ldf = null
         }
-      } else {
-        draft.databaseMetadata {
-          mrd = datetime.now()
-          rlv?.also { rlv = it + 1 }
-        }
+      }
+    } else {
+      draft.databaseMetadata {
+        mrd = datetime.now()
+        rlv?.also { rlv = it + 1 }
       }
     }
   }
