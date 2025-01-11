6034b5c286c58d6fc94a6bc5493f4fae35f7eab4
赵日天 <truenine304520@gmail.com>
2025-01-11 11:08:37 +0800

    refactor(rds): 更新 JimmerSnowflakeLongIdGenerator 类
    - 将 UserIdGenerator 的泛型从 Long 改为 RefId
    - 添加导入 RefId 类

--- rds/core/src/main/kotlin/net/yan100/compose/rds/core/generators/JimmerSnowflakeLongIdGenerator.kt
+++ rds/core/src/main/kotlin/net/yan100/compose/rds/core/generators/JimmerSnowflakeLongIdGenerator.kt
@@ -1,11 +1,12 @@
 package net.yan100.compose.rds.jimmer.generators
 
+import net.yan100.compose.core.RefId
 import net.yan100.compose.core.generator.ISnowflakeGenerator
 import org.babyfish.jimmer.sql.meta.UserIdGenerator
 
 class JimmerSnowflakeLongIdGenerator(
   private var snowflake: ISnowflakeGenerator
-) : UserIdGenerator<Long> {
+) : UserIdGenerator<RefId> {
   companion object {
     const val JIMMER_SNOWFLAKE_LONG_ID_GENERATOR_NAME = "jimmerSnowflakeLongIdGenerator"
   }
