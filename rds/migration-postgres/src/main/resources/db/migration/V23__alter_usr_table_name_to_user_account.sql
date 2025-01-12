04fa4ae5cb3838a2bd37d7f175f6ebb9b2483a0b
赵日天 <truenine304520@gmail.com>
2025-01-12 17:25:25 +0800

    build(deps): 更新 Compose相关依赖版本
    - 更新 Compose 版本目录至 2.0.130
    - 升级 Compose Postgres迁移版本至 0.0.13
    - 优化 Postgres 迁移脚本，增加表存在性检查

--- rds/migration-postgres/src/main/resources/db/migration/V23__alter_usr_table_name_to_user_account.sql
+++ rds/migration-postgres/src/main/resources/db/migration/V23__alter_usr_table_name_to_user_account.sql
@@ -5,7 +5,15 @@ do $$ begin if exists(
         information_schema.tables as t
     where
         t.table_name = 'usr'
+)
+and not exists(
+    select
+        1
+    from
+        information_schema.tables as t
+    where
+        t.table_name = 'user_account'
 ) then alter table
-    usr rename to user_account;
+    if exists usr rename to user_account;
 end if;
 end $$;
