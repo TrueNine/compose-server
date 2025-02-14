742b0dbba74df1b18726981f4d1a8606d142c1b3
赵日天 <truenine304520@gmail.com>
2025-02-15 00:33:30 +0800

    build(depend): 更新 Paho MQTT客户端版本并升级 Spring相关依赖
    - 在 paho/build.gradle.kts 中添加了 org.eclipse.paho.client.mqttv3 依赖
    - 更新了 gradle/libs.versions.toml 中的 Spring Boot、Spring Data 和 Spring Security版本
    - 升级了 compose 依赖版本

--- depend/paho/build.gradle.kts
+++ depend/paho/build.gradle.kts
@@ -6,6 +6,7 @@ version = libs.versions.composeDependPaho.get()
 
 dependencies {
   implementation(libs.org.springframework.integration.springIntegrationMqtt)
+  implementation(libs.org.eclipse.paho.client.mqttv3)
   implementation(libs.com.fasterxml.jackson.core.jacksonDatabind)
   implementation(projects.core)
 
