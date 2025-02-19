fe37edf2706702837c65c41a92b8f78e8c417649
赵日天 <truenine304520@gmail.com>
2025-02-19 20:37:21 +0800

    test(rds): 添加 Duration 和 Period 标量提供者单元测试
    - 新增 DurationScalarProviderTest 类，实现对 Duration 标量提供者的测试
    - 新增 PeriodScalarProviderTest 类，实现对 Period标量提供者的测试
    - 测试内容包括有效性检查、异常处理、零值处理等

---
+++ rds/core/src/test/kotlin/net/yan100/compose/rds/core/converters/jimmer/DurationScalarProviderTest.kt
@@ -0,0 +1,38 @@
+package net.yan100.compose.rds.core.converters.jimmer
+
+import net.yan100.compose.testtookit.log
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.Duration
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+@SpringBootTest
+class DurationScalarProviderTest {
+  private val durationStr = "PT336H"
+  private val duration = Duration.parse(durationStr)
+
+  @Test
+  fun `测试 duration 的可转换性`() {
+    assertEquals(duration.toDays(), 14)
+    assertEquals(duration.toHours(), 14 * 24)
+    val result = duration.toString()
+    log.info(result)
+  }
+
+  @Test
+  fun toScalar() {
+    val provider = DurationScalarProvider()
+    val result = provider.toScalar(durationStr)
+    assertNotNull(result)
+    assertEquals(result, duration)
+  }
+
+  @Test
+  fun toSql() {
+    val provider = DurationScalarProvider()
+    val result = provider.toSql(duration)
+    assertNotNull(result)
+    assertEquals(result, durationStr)
+  }
+}
