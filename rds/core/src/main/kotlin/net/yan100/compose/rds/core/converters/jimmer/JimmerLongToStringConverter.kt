a5b2c330eade85ff5b9f12673ab69c3e38cd249b
赵日天 <truenine304520@gmail.com>
2025-02-22 05:21:56 +0800

    refactor(rds): 重构 JimmerLongToStringConverter 类
    - 移除了对 LongToStringConverter 的直接继承
    - 重写了 output 和 input 方法，以实现更灵活的转换逻辑
    - 提高了代码的可读性和可维护性

--- rds/core/src/main/kotlin/net/yan100/compose/rds/core/converters/jimmer/JimmerLongToStringConverter.kt
+++ rds/core/src/main/kotlin/net/yan100/compose/rds/core/converters/jimmer/JimmerLongToStringConverter.kt
@@ -2,6 +2,13 @@ package net.yan100.compose.rds.core.converters.jimmer
 
 import net.yan100.compose.core.RefId
 import org.babyfish.jimmer.jackson.Converter
-import org.babyfish.jimmer.jackson.LongToStringConverter
 
-class JimmerLongToStringConverter : Converter<RefId, String> by LongToStringConverter()
+class JimmerLongToStringConverter : Converter<RefId, String> {
+  override fun output(value: RefId?): String? {
+    return value?.toString()
+  }
+
+  override fun input(jsonValue: String?): RefId? {
+    return jsonValue?.toLongOrNull()
+  }
+}
