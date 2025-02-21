bc0202747db0934180f328c54de3f38ce561a79b
赵日天 <truenine304520@gmail.com>
2025-02-21 23:06:58 +0800

    feat(core): 添加元数据转换器并更新实体接口
    - 新增 NullDbMetadataConverter 和 NullDbTreeMetadataConverter 类
    - 在 IJimmerEntity 和 IJimmerTreeEntity 接口中添加元数据字段并使用转换器
    - 更新实体接口文档，明确元数据字段的用途和行为

--- rds/core/src/main/kotlin/net/yan100/compose/rds/core/entities/IJimmerEntity.kt
+++ rds/core/src/main/kotlin/net/yan100/compose/rds/core/entities/IJimmerEntity.kt
@@ -1,12 +1,17 @@
 package net.yan100.compose.rds.core.entities
 
+import net.yan100.compose.rds.core.converters.jimmer.NullDbMetadataConverter
 import net.yan100.compose.rds.core.models.IDbMetadata
+import org.babyfish.jimmer.jackson.JsonConverter
 import org.babyfish.jimmer.sql.MappedSuperclass
 
 @MappedSuperclass
 interface IJimmerEntity : IJimmerPersistentEntity {
   /**
    * 数据库内元数据
+   *
+   * > 不能传入，也不会输出，不应干预
    */
+  @JsonConverter(NullDbMetadataConverter::class)
   val databaseMetadata: IDbMetadata?
 }
--- rds/core/src/main/kotlin/net/yan100/compose/rds/core/entities/IJimmerTreeEntity.kt
+++ rds/core/src/main/kotlin/net/yan100/compose/rds/core/entities/IJimmerTreeEntity.kt
@@ -1,9 +1,17 @@
 package net.yan100.compose.rds.core.entities
 
+import net.yan100.compose.rds.core.converters.jimmer.NullDbTreeMetadataConverter
 import net.yan100.compose.rds.core.models.IDbTreeMetadata
+import org.babyfish.jimmer.jackson.JsonConverter
 import org.babyfish.jimmer.sql.MappedSuperclass
 
 @MappedSuperclass
 interface IJimmerTreeEntity : IJimmerEntity {
-  val databaseTreeMetadata: IDbTreeMetadata
+  /**
+   * 树节点元数据
+   *
+   * > 不能传入，也不会输出，不应干预
+   */
+  @JsonConverter(NullDbTreeMetadataConverter::class)
+  val databaseTreeMetadata: IDbTreeMetadata?
 }
