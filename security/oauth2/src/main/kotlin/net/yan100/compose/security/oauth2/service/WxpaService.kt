b57bb1b1b0352ac13818bc82f3d05365a8108641
赵日天 <truenine304520@gmail.com>
2025-02-23 07:57:54 +0800

    feat(security.oauth2): 添加微信公众号基本配置验证和签名功能
    - 新增 WxpaVerifyDto 数据类用于微信公众平台验证请求- 实现 verifyBasicConfig 和 verifyBasicConfigOrThrow 方法用于验证微信公众平台基本配置信息
    - 添加 fetchJsApiSignature 方法用于为微信 JS API 签名
    -优化 WxpaService 类，增加公众号配置信息属性

--- security/oauth2/src/main/kotlin/net/yan100/compose/security/oauth2/service/WxpaService.kt
+++ security/oauth2/src/main/kotlin/net/yan100/compose/security/oauth2/service/WxpaService.kt
@@ -1,6 +1,8 @@
 package net.yan100.compose.security.oauth2.service
 
+import net.yan100.compose.core.hasText
 import net.yan100.compose.core.slf4j
+import net.yan100.compose.security.crypto.sha1
 import net.yan100.compose.security.oauth2.api.IWxpaWebClient
 import net.yan100.compose.security.oauth2.property.WxpaProperty
 import org.springframework.stereotype.Service
@@ -18,6 +20,61 @@ class WxpaService(
   private val client: IWxpaWebClient,
   private val property: WxpaProperty
 ) {
+  data class WxpaVerifyDto(
+    var preValidToken: String,
+    var timestamp: Int,
+    var nonce: String,
+    var signature: String,
+    var echostr: String
+  )
+
+  /**
+   * ## 公众号配置信息
+   */
+  val wxpaConfigInfo get() = property
+
+  /**
+   * ## 验证微信公众平台的基本配置信息
+   * > 如果验证失败则抛出异常。
+   *
+   * @param body 包含微信公众平台验证请求的DTO对象
+   * @return 返回验证成功后的响应字符串
+   * @throws IllegalStateException 如果验证失败则抛出异常
+   */
+  fun verifyBasicConfigOrThrow(
+    body: WxpaVerifyDto
+  ): String {
+    return verifyBasicConfig(body) ?: error("verifyBasicConfig failed")
+  }
+
+  /**
+   * ## 验证微信公众平台的基本配置信息
+   * > 如果验证失败则返回 null
+   *
+   * @param body 包含微信公众平台验证请求的DTO对象
+   * @return 返回验证成功后的响应字符串
+   */
+  fun verifyBasicConfig(
+    body: WxpaVerifyDto
+  ): String? {
+    val a = listOf(wxpaConfigInfo.preValidToken!!, body.timestamp.toString(), body.nonce).sorted().joinToString("").sha1
+    log.trace("gen sig = {}, body.sign = {}", a, body.signature)
+    return body.echostr
+  }
+
+  /**
+   * ## 为 js api 签名
+   *
+   * @param encodedUrl 编码后的 url
+   * @param nonceString 随机字符串
+   */
+  fun fetchJsApiSignature(
+    encodedUrl: String,
+    nonceString: String?
+  ): WxpaProperty.WxpaSignatureResp {
+    return if (nonceString.hasText()) wxpaConfigInfo.signature(encodedUrl, nonceString) else wxpaConfigInfo.signature(encodedUrl)
+  }
+
   /**
    * ## 获取公众号用户信息
    * @param jsCode jsCode
