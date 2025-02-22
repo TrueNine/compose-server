8590f18edfeb8d75635b114cb4ea4e97a3419cfe
赵日天 <truenine304520@gmail.com>
2025-02-23 07:35:48 +0800

    feat(core): 添加 AuthRequestInfo 类的 isLogin 扩展属性
    当前是否已经完全登录- 新增 isLogin 扩展属性用于判断用户是否已登录
    
    - 登录条件包括：userId 不为空、enabled 为 true、nonLocked 为 true、nonExpired为 true 以及 account 不为空

---
+++ core/src/main/kotlin/net/yan100/compose/core/domain/RequestInfoFns.kt
@@ -0,0 +1,11 @@
+package net.yan100.compose.core.domain
+
+/**
+ * ## 当前是否已经完全登录
+ */
+val AuthRequestInfo.isLogin
+  get() = userId != null
+    && enabled
+    && nonLocked
+    && nonExpired
+    && account != null
