ebe191ec6ff34f7ea55f239ddb54f9f175f3bb97
赵日天 <truenine304520@gmail.com>
2025-02-24 04:36:33 +0800

    build(bom): 从版本目录自动添加依赖项
    - 引入 kotlin.jvm.optionals.getOrNull 函数
    - 使用 VersionCatalogsExtension 自动遍历和添加依赖项
    - 替换手动添加依赖项的方式，提高代码可维护性

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -1,3 +1,5 @@
+import kotlin.jvm.optionals.getOrNull
+
 plugins {
   `java-platform`
   `publish-convention`
@@ -7,34 +9,13 @@ version = libs.versions.compose.asProvider().get()
 
 dependencies {
   constraints {
-    //api(projects.gradlePlugin)
-    api(projects.cacheable)
-    api(projects.core)
-    api(projects.client)
-    api(projects.data.dataExtract)
-    api(projects.data.dataCrawler)
-    api(projects.depend.dependHttpExchange)
-    api(projects.depend.dependJackson)
-    api(projects.depend.dependPaho)
-    api(projects.depend.dependServlet)
-    api(projects.depend.dependSpringdocOpenapi)
-    api(projects.depend.dependXxlJob)
-
-    api(projects.ksp.kspClient)
-    api(projects.ksp.kspPlugin)
-    api(projects.ksp.kspToolkit)
-    api(projects.meta)
-    api(projects.oss)
-    api(projects.pay)
-    api(projects.rds.rdsCore)
-    api(projects.rds.rdsCrud)
-    api(projects.rds.rdsMigrationH2)
-    api(projects.rds.rdsMigrationMysql)
-    api(projects.rds.rdsMigrationPostgres)
-    api(projects.security.securityCrypto)
-    api(projects.security.securityOauth2)
-    api(projects.security.securitySpring)
-    api(projects.testtoolkit)
-    api(projects.versionCatalog)
+    val allVersionCatalogs = rootProject.extensions.getByType<VersionCatalogsExtension>().named("libs")
+    allVersionCatalogs.libraryAliases.forEach { aliasLib ->
+      val dependency = allVersionCatalogs.findLibrary(aliasLib).getOrNull()?.get()
+      dependency?.also { dep ->
+        api(dep)
+        logger.debug("add dependency: {}", dep)
+      }
+    }
   }
 }
