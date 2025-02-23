68ea6f523d1f05c624a727cbc10b20ad22cd59cc
赵日天 <truenine304520@gmail.com>
2025-02-24 04:37:31 +0800

    build(version-catalog): 添加第三方库的 compileOnly 依赖
    - 在 version-catalog 子项目中添加 java 和 repositories-convention 插件
    - 遍历 libs 版本目录中的所有库别名
    - 对于非 net.yan100.compose 前缀的库，添加 compileOnly依赖
    - 优化版本目录的加载方式

--- version-catalog/build.gradle.kts
+++ version-catalog/build.gradle.kts
@@ -1,8 +1,24 @@
+import kotlin.jvm.optionals.getOrNull
+
 plugins {
+  java
+  `repositories-convention`
   `version-catalog`
   `publish-convention`
 }
 
 version = libs.versions.compose.asProvider().get()
+
+dependencies {
+  val allVersionCatalogs = rootProject.extensions.getByType<VersionCatalogsExtension>().named("libs")
+  allVersionCatalogs.libraryAliases.forEach { aliasLib ->
+    val dependency = allVersionCatalogs.findLibrary(aliasLib).getOrNull()?.get()
+    dependency?.also { d ->
+      if (d.group?.contains("net.yan100.compose") == false) {
+        compileOnly(d)
+      }
+    }
+  }
+}
 
 catalog { versionCatalog { from(files("../gradle/libs.versions.toml")) } }
