dedac3345d98b3e7c1a87c433fbde62202ff6af1
赵日天 <truenine304520@gmail.com>
2025-02-25 03:30:04 +0800

    refactor(rds-core): 移除不必要的 Jimmer 转换器并优化 Long 到 String 的转换
    - 移除了 NullDbMetadataConverter 和 NullDbTreeMetadataConverter，因为它们没有实际的转换逻辑
    - 优化了 JimmerLongToStringConverter，移除了不必要的空值处理

--- rds/core/src/main/kotlin/net/yan100/compose/rds/core/converters/jimmer/JimmerLongToStringConverter.kt
+++ rds/core/src/main/kotlin/net/yan100/compose/rds/core/converters/jimmer/JimmerLongToStringConverter.kt
@@ -4,11 +4,11 @@ import net.yan100.compose.core.RefId
 import org.babyfish.jimmer.jackson.Converter
 
 class JimmerLongToStringConverter : Converter<RefId, String> {
-  override fun output(value: RefId?): String? {
-    return value?.toString()
+  override fun output(value: RefId): String {
+    return value.toString()
   }
 
-  override fun input(jsonValue: String?): RefId? {
-    return jsonValue?.toLongOrNull()
+  override fun input(jsonValue: String): RefId {
+    return jsonValue.toLong()
   }
 }
