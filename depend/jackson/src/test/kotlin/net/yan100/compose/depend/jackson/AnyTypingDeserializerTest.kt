b0278e2e14495120ede5d2f9b6808510c12f43b0
赵日天 <truenine304520@gmail.com>
2025-02-25 07:02:57 +0800

    test(depend-jackson): 更新 AnyTypingDeserializer 测试逻辑
    - 修改了测试用例名称，更准确地反映了测试目的
    - 引入了 assertFailsWith 函数，用于验证枚举类型撤销后的行为
    - 更新了 JSON 断言，以匹配新的预期结果

--- depend/jackson/src/test/kotlin/net/yan100/compose/depend/jackson/AnyTypingDeserializerTest.kt
+++ depend/jackson/src/test/kotlin/net/yan100/compose/depend/jackson/AnyTypingDeserializerTest.kt
@@ -24,6 +24,7 @@ import net.yan100.compose.testtookit.log
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
 
 
@@ -32,14 +33,20 @@ class AnyTypingDeserializerTest {
   lateinit var mapper: ObjectMapper @Resource set
 
   @Test
-  fun `test deserializer`() {
+  fun `确保 撤销了枚举类型后，测试逻辑仍然可以运行`() {
     val stringTyping = UserAgents.CHROME_WIN_103
     val intTyping = HttpStatusTyping._403
     val dd = AnyTypingRecord(stringTyping, intTyping)
     val json = mapper.writeValueAsString(dd)
     log.info("json: {}", json)
+    assertFailsWith<AssertionError> {
+      assertEquals(
+        """{"stringTyping1":"${UserAgents.CHROME_WIN_103.value}","intTyping2":${HttpStatusTyping._403.value}}""",
+        json
+      )
+    }
     assertEquals(
-      """{"stringTyping1":"${UserAgents.CHROME_WIN_103.value}","intTyping2":${HttpStatusTyping._403.value}}""",
+      """{"stringTyping1":"CHROME_WIN_103","intTyping2":"_403"}""",
       json
     )
     val des = mapper.readValue(json, AnyTypingRecord::class.java)
