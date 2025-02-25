0f3022521419f922f1e32c7b1da43447c6052c01
赵日天 <truenine304520@gmail.com>
2025-02-26 02:25:29 +0800

    test(testtoolkit): 更新 LaunchTest 以验证环境变量读取
    - 重命名测试函数以更清晰地表达测试目的
    - 添加两个新的测试用例来验证环境变量的读取
    - 优化导入语句，使用通配符导入 kotlin.test 包

--- testtoolkit/src/test/kotlin/net/yan100/compose/testtookit/LaunchTest.kt
+++ testtoolkit/src/test/kotlin/net/yan100/compose/testtookit/LaunchTest.kt
@@ -1,8 +1,6 @@
 package net.yan100.compose.testtookit
 
-import kotlin.test.BeforeTest
-import kotlin.test.Test
-import kotlin.test.assertNotNull
+import kotlin.test.*
 
 /**
  * 确保能够启动
@@ -16,7 +14,20 @@ class LaunchTest {
   }
 
   @Test
-  fun `test launch`() {
+  fun `gradle 读取到了 project env 文件`() {
+    val e = System.getenv("TEST_A")
+    assertNotNull(e)
+    assertEquals(e, "1")
+  }
+
+  @Test
+  fun `确保 能够 加载到 gradle 环境变量`() {
+    val e = System.getenv("ENV_FILE_EMPTY")
+    assertNull(e)
+  }
+
+  @Test
+  fun `确保 测试类 能够 正常启动`() {
     log.info("launched")
   }
 }
