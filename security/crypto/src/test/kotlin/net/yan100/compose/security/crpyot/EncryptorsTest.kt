d5c88f6024712461f4720399461b9a0ebbbf3c98
赵日天 <truenine304520@gmail.com>
2025-02-27 13:42:14 +0800

    test(security): 添加 Encryptors 类的单元测试
    - 新增 EncryptorsTest 类，包含一个测试 SHA-256 加密的测试用例
    - 验证 Encryptors.signatureBySha256 方法的正确性
    - 使用日志记录加密结果

---
+++ security/crypto/src/test/kotlin/net/yan100/compose/security/crpyot/EncryptorsTest.kt
@@ -0,0 +1,19 @@
+package net.yan100.compose.security.crpyot
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import net.yan100.compose.security.crypto.Encryptors
+import net.yan100.compose.testtookit.log
+
+class EncryptorsTest {
+
+  @Test
+  fun `测试 sha-256 加密`() {
+    val sha256signature = Encryptors.signatureBySha256("hello world")
+    log.info("sha256 signature: {}", sha256signature)
+    assertEquals(
+      sha256signature,
+      "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9",
+    )
+  }
+}
