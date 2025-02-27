9ec8a5f135988b0dbb9263807fc18d2b9346542d
赵日天 <truenine304520@gmail.com>
2025-02-27 10:32:11 +0800

    refactor(core): 重构 Id相关函数并增强其健壮性
    - 将 isId() 函数扩展为 Any? 类型，增加对非 Id 类型的判断
    - 修改 getDefaultNullableId() 函数的可见性，限制其使用范围
    - 重构 toId() 函数，支持多种类型输入并进行空值和异常处理
    - 新增 Number 类型的 toId() 扩展函数
    - 新增通用 Any? 类型的 toId() 函数，统一处理各种输入类型

--- core/src/main/kotlin/net/yan100/compose/core/AliasFns.kt
+++ core/src/main/kotlin/net/yan100/compose/core/AliasFns.kt
@@ -5,18 +5,28 @@ import kotlin.contracts.contract
 
 /** ## 判断当前 Id 类型是否不为空 */
 @OptIn(ExperimentalContracts::class)
-fun Id?.isId(): Boolean {
-  contract { returns(true) implies (this@isId != null) }
-  return this !== null && this != Long.MIN_VALUE
+fun Any?.isId(): Boolean {
+  contract { returns(true) implies (this@isId is Id) }
+  return this != Long.MIN_VALUE && this is Id
 }
 
 @Deprecated("框架内部调用代码，不应由用户直接调用", level = DeprecationLevel.ERROR)
 inline fun getDefaultNullableId(): Id = Long.MIN_VALUE
 
-fun String.toId(): Id {
-  return this.toLong()
+fun String.toId(): Id? {
+  return this.toLongOrNull()
 }
 
-fun Int.toId(): Id {
-  return this.toLong()
+fun Number.toId(): Id? {
+  return this.toLong().takeIf { it != Long.MIN_VALUE }
+}
+
+fun Any?.toId(): Id? {
+  return when (this) {
+    is Id -> this
+    is Number -> this.toId()
+    is String -> this.toId()
+    null -> null
+    else -> null
+  }
 }
