f7b5d1e2219c6e2622c6b9c15506f5a494c6aee5
赵日天 <truenine304520@gmail.com>
2025-03-01 08:56:33 +0800

    feat(rds): 重构添加基础结构的函数并添加新功能
    - 重构 add_base_struct 函数，使用数组存储列定义，提高可维护性
    - 添加 rm_base_struct 函数，用于删除基础结构
    - 添加 all_to_nullable 函数，将所有列设为可空
    - 优化代码结构，提高可读性和可维护性

--- rds/migration-postgres/src/main/resources/db/migration/V3__create_postgres_helper_functions_add_base_struct.sql
+++ rds/migration-postgres/src/main/resources/db/migration/V3__create_postgres_helper_functions_add_base_struct.sql
@@ -1,87 +1,123 @@
 create
     or replace function add_base_struct(
         tab_name varchar(128)
+    ) returns void as $$ declare column_definitions text [][]:= array [[ 'id',
+    'bigint not null',
+    'add primary key (id)' ],
+    [ 'rlv',
+    'bigint default 0',
+    null ],
+    [ 'crd',
+    'timestamp default now()',
+    null ],
+    [ 'mrd',
+    'timestamp default now()',
+    null ],
+    [ 'ldf',
+    'bool default null',
+    null ]];
+
+existing_column_name text;
+
+column_definition text [];
+
+begin foreach column_definition slice 1 in array column_definitions loop select
+    column_name into
+        existing_column_name
+    from
+        information_schema.columns
+    where
+        table_name = tab_name
+        and column_name = column_definition [ 1 ];
+
+if existing_column_name is null then execute format(
+    'alter table if exists %I add column if not exists %I %s;',
+    tab_name,
+    column_definition [ 1 ],
+    column_definition [ 2 ]
+);
+
+if column_definition [ 3 ] is not null then execute format(
+    'alter table if exists %I %s;',
+    tab_name,
+    column_definition [ 3 ]
+);
+end if;
+end if;
+end loop;
+end;
+
+$$ language plpgsql;
+
+create
+    or replace function rm_base_struct(
+        tab_name varchar(128)
     ) returns void as $$ declare existing_column_name text;
 
+pk_constraint_name text;
+
 begin select
+    constraint_name into
+        pk_constraint_name
+    from
+        information_schema.table_constraints
+    where
+        table_name = tab_name
+        and constraint_type = 'PRIMARY KEY';
+
+if pk_constraint_name is not null then execute format(
+    'alter table if exists %I drop constraint if exists %I;',
+    tab_name,
+    pk_constraint_name
+);
+end if;
+
+foreach existing_column_name in array array [ 'id',
+'rlv',
+'crd',
+'mrd',
+'ldf' ] loop select
     column_name into
         existing_column_name
     from
         information_schema.columns
     where
         table_name = tab_name
-        and column_name = 'id';
+        and column_name = existing_column_name;
 
-if existing_column_name is null then execute format(
-    'alter table if exists %I add column if not exists id bigint not null;',
-    tab_name
-);
-
-execute format(
-    'alter table if exists %I add primary key (id);',
-    tab_name
+if existing_column_name is not null then execute format(
+    'alter table if exists %I drop column if exists %I;',
+    tab_name,
+    existing_column_name
 );
 end if;
+end loop;
+end;
 
-select
-    column_name into
-        existing_column_name
-    from
-        information_schema.columns
-    where
-        table_name = tab_name
-        and column_name = 'rlv';
+$$ language plpgsql;
 
-if existing_column_name is null then execute format(
-    'alter table if exists %I add column if not exists rlv bigint default 0;',
-    tab_name
-);
+create
+    or replace function all_to_nullable(
+        tab_name varchar(128)
+    ) returns void as $$ declare column_info record;
+
+begin for column_info in select
+    column_name,
+    data_type,
+    is_nullable,
+    column_default
+from
+    information_schema.columns
+where
+    table_name = tab_name loop if column_info.is_nullable = 'NO'
+    or column_info.column_default is not null then execute format(
+        'ALTER TABLE %I ALTER COLUMN %I DROP DEFAULT, ALTER COLUMN %I DROP NOT NULL;',
+        tab_name,
+        column_info.column_name,
+        column_info.column_name
+    );
 end if;
-
-select
-    column_name into
-        existing_column_name
-    from
-        information_schema.columns
-    where
-        table_name = tab_name
-        and column_name = 'crd';
-
-if existing_column_name is null then execute format(
-    'alter table if exists %I add column if not exists crd timestamp default now();',
-    tab_name
-);
-end if;
-
-select
-    column_name into
-        existing_column_name
-    from
-        information_schema.columns
-    where
-        table_name = tab_name
-        and column_name = 'mrd';
-
-if existing_column_name is null then execute format(
-    'alter table if exists %I add column if not exists mrd timestamp default now();',
-    tab_name
-);
-end if;
-
-select
-    column_name into
-        existing_column_name
-    from
-        information_schema.columns
-    where
-        table_name = tab_name
-        and column_name = 'ldf';
-
-if existing_column_name is null then execute format(
-    'alter table if exists %I add column if not exists ldf bool default null;',
-    tab_name
-);
-end if;
+end loop;
 end;
 
 $$ language plpgsql;
