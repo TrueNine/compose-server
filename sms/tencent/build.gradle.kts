43dac0ec6c9e85e7e1c76cb83af8254cc74189a2
赵日天 <truenine304520@gmail.com>
2025-03-05 23:55:48 +0800

    test(sms): 添加腾讯云短信发送测试用例
    - 新增 TencentSmsTest 类，包含一个忽略的测试方法
    - 测试方法验证了腾讯云短信发送的基本流程
    - 更新 build.gradle.kts，添加测试依赖和排除不必要的库

--- sms/tencent/build.gradle.kts
+++ sms/tencent/build.gradle.kts
@@ -1,7 +1,21 @@
-plugins { `kotlinspring-convention` }
+plugins {
+  `kotlinspring-convention`
+}
 
 version = libs.versions.compose.sms.tencent.get()
 
 dependencies {
-  api(libs.com.tencentcloudapi.tencentcloud.sdk)
+  api(libs.com.tencentcloudapi.tencentcloud.sdk.java.sms) {
+    exclude(
+      group = libs.com.squareup.okio.okio.get().module.group,
+      module = libs.com.squareup.okio.okio.get().module.name,
+    )
+    exclude(
+      group = libs.com.squareup.okhttp3.okhttp.get().module.group,
+      module = libs.com.squareup.okhttp3.okhttp.get().module.name,
+    )
+  }
+  implementation(libs.com.squareup.okhttp3.okhttp)
+  
+  testImplementation(projects.testtoolkit)
 }
---
+++ sms/tencent/src/test/kotlin/net/yan100/compose/sms/TencentSmsTest.kt
@@ -0,0 +1,52 @@
+package net.yan100.compose.sms
+
+import com.tencentcloudapi.common.Credential
+import com.tencentcloudapi.common.profile.ClientProfile
+import com.tencentcloudapi.common.profile.Language
+import com.tencentcloudapi.sms.v20210111.SmsClient
+import com.tencentcloudapi.sms.v20210111.models.SendSmsRequest
+import com.tencentcloudapi.sms.v20210111.models.SendSmsResponse
+import kotlin.test.*
+
+
+class TencentSmsTest {
+
+  @Ignore
+  @Test
+  fun `腾讯云发送短信 太垃圾了，没发出去`() {
+    val sdkAppId = System.getenv("TENCENTCLOUD_SDK_APP_ID")
+    val secretId = System.getenv("TENCENTCLOUD_SECRET_ID")
+    val secretKey = System.getenv("TENCENTCLOUD_SECRET_KEY")
+
+    assertNotNull(sdkAppId)
+    assertNotNull(secretId)
+    assertNotNull(secretKey)
+
+    val cred = Credential(secretId, secretKey)
+
+    val clientProfile = ClientProfile()
+    clientProfile.language = Language.ZH_CN
+    clientProfile.isDebug = true
+
+    val client = SmsClient(cred, "ap-beijing", clientProfile)
+
+    val req = SendSmsRequest()
+    req.smsSdkAppId = sdkAppId
+    req.signName = "湖南募残信息科技"
+    req.templateId = "2373465"
+    req.templateParamSet = arrayOf("134235")
+    req.phoneNumberSet = arrayOf("+8619918540858")
+
+    val res = client.SendSms(req)
+    println(SendSmsResponse.toJsonString(res))
+
+    val statusSet = res.sendStatusSet
+    assertNotNull(statusSet)
+    assertTrue { statusSet.size > 0 }
+    assertNotNull(res.requestId)
+    statusSet.forEach {
+      assertNotEquals(it.code, "FailedOperation.SignatureIncorrectOrUnapproved", "签名不正确或未审核通过")
+      assertContains(it.code, "Ok")
+    }
+  }
+}
