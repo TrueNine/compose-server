e3a8e2352632ec0237a88a978b716b68801816c2
赵日天 <truenine304520@gmail.com>
2025-03-14 04:59:56 +0800

    build:清理 oss 模块代码
    - 删除了 AliCloudOss、MinioClientAdaptor、OssAutoConfiguration 等多个类
    - 移除了 oss 模块的大部分功能实现- 清理了相关配置类和属性

--- oss/src/main/kotlin/net/yan100/compose/oss/properties/AliCloudOssProperties.kt
+++
@@ -1,9 +0,0 @@
-package net.yan100.compose.oss.properties
-
-class AliCloudOssProperties {
-  var enable = false
-  var accessKey: String? = null
-  var endpoint: String? = null
-  var accessKeySecret: String? = null
-  var bucketName: String? = null
-}
