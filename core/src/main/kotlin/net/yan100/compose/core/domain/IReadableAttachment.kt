732757d4315ac5ab5698bbe5878685ac63c22468
赵日天 <truenine304520@gmail.com>
2025-03-14 04:58:21 +0800

    refactor(core): 重构附件实体并添加可写附件接口
    - 重构 IReadableAttachment 接口，优化属性顺序和类型
    - 新增 IWriteableAttachment 接口，用于表示可写附件
    - 添加 DefaultWriteableAttachment 和 EmptyWriteableAttachment 类
    - 优化 equals 和 hashCode 方法的实现

--- core/src/main/kotlin/net/yan100/compose/core/domain/IReadableAttachment.kt
+++ core/src/main/kotlin/net/yan100/compose/core/domain/IReadableAttachment.kt
@@ -1,10 +1,10 @@
 package net.yan100.compose.core.domain
 
-import java.io.InputStream
-import java.io.Serializable
 import net.yan100.compose.core.bool
 import net.yan100.compose.core.i64
 import net.yan100.compose.core.string
+import java.io.InputStream
+import java.io.Serializable
 
 /**
  * # 附件表示形式
@@ -19,7 +19,7 @@ interface IReadableAttachment : Serializable {
 
   val empty: bool
   val size: i64
-  val bytes: ByteArray?
+  val bytes: (() -> ByteArray?)?
     get() = null
 
   val inputStream: InputStream?
@@ -37,56 +37,31 @@ interface IReadableAttachment : Serializable {
     override val mimeType: string? = null,
     override val empty: bool = false,
     override val size: i64 = 0L,
-    override val bytes: ByteArray? = null,
+    override val bytes: (() -> ByteArray?)? = null,
     override val inputStream: InputStream? = null,
   ) : IReadableAttachment {
     override fun equals(other: Any?): Boolean {
       if (this === other) return true
-      if (javaClass != other?.javaClass) return false
-      other as DefaultReadableAttachment
-      if (name != other.name) return false
-      if (mimeType != other.mimeType) return false
+      if (other !is DefaultReadableAttachment) return false
       if (empty != other.empty) return false
       if (size != other.size) return false
-      if (bytes != null) {
-        if (other.bytes == null) return false
-        if (!bytes.contentEquals(other.bytes)) return false
-      } else if (other.bytes != null) return false
-      if (inputStream != other.inputStream) return false
+      if (name != other.name) return false
+      if (mimeType != other.mimeType) return false
       return true
     }
 
     override fun hashCode(): Int {
-      var result = name.hashCode()
-      result = 31 * result + (mimeType?.hashCode() ?: 0)
-      result = 31 * result + empty.hashCode()
+      var result = empty.hashCode()
       result = 31 * result + size.hashCode()
-      result = 31 * result + (bytes?.contentHashCode() ?: 0)
-      result = 31 * result + (inputStream?.hashCode() ?: 0)
+      result = 31 * result + name.hashCode()
+      result = 31 * result + (mimeType?.hashCode() ?: 0)
       return result
     }
   }
 
   companion object {
-    operator fun get(
-      name: string,
-      mimeType: string?,
-      isEmpty: bool,
-      size: i64,
-      bytes: ByteArray? = null,
-      inputStream: InputStream? = null,
-    ): IReadableAttachment =
-      DefaultReadableAttachment(
-        name,
-        mimeType,
-        isEmpty,
-        size,
-        bytes,
-        inputStream,
-      )
-
-    fun empty(): IReadableAttachment {
-      return EmptyReadableAttachment("")
+    fun empty(name: string = ""): IReadableAttachment {
+      return EmptyReadableAttachment(name = name)
     }
   }
 }
---
+++ core/src/main/kotlin/net/yan100/compose/core/domain/IWriteableAttachment.kt
@@ -0,0 +1,58 @@
+package net.yan100.compose.core.domain
+
+import net.yan100.compose.core.bool
+import net.yan100.compose.core.i64
+import java.io.OutputStream
+
+interface IWriteableAttachment {
+  val name: String
+  val outputStream: OutputStream?
+  val empty: bool
+  val size: i64
+  val bytes: (() -> ByteArray?)?
+    get() = null
+  val mimeType: String?
+    get() = null
+
+  data class EmptyWriteableAttachment(
+    override val name: String,
+    override val outputStream: OutputStream? = null,
+    override val empty: bool = true,
+    override val size: i64 = 0
+  ) : IWriteableAttachment
+
+
+  data class DefaultWriteableAttachment(
+    override val name: String,
+    override val mimeType: String? = null,
+    override val outputStream: OutputStream?,
+    override val size: i64,
+    override val empty: bool = size > 0
+  ) : IWriteableAttachment {
+    override fun equals(other: Any?): Boolean {
+      if (this === other) return true
+      if (other !is DefaultWriteableAttachment) return false
+      if (size != other.size) return false
+      if (empty != other.empty) return false
+      if (name != other.name) return false
+      if (mimeType != other.mimeType) return false
+      return true
+    }
+
+    override fun hashCode(): Int {
+      var result = size.hashCode()
+      result = 31 * result + empty.hashCode()
+      result = 31 * result + name.hashCode()
+      result = 31 * result + (mimeType?.hashCode() ?: 0)
+      return result
+    }
+  }
+
+
+  companion object {
+    @JvmStatic
+    fun empty(name: String = ""): IWriteableAttachment {
+      return EmptyWriteableAttachment(name)
+    }
+  }
+}
