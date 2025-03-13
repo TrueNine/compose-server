ccc331b5c593d380beb92d75121dd6398a550f97
赵日天 <truenine304520@gmail.com>
2025-03-14 05:00:23 +0800

    refactor(servlet): 重构 MultipartFile.toReadableAttachment() 扩展函数
    - 使用 IReadableAttachment.DefaultReadableAttachment 替代原始的 IReadableAttachment[...] 构造方式
    - 更新参数命名和顺序，以适应新的构造函数
    -移除了对 MimeTypes.BINARY.name 的引用，直接使用 contentType

--- depend/servlet/src/main/kotlin/net/yan100/compose/depend/servlet/IReadableAttachmentFns.kt
+++ depend/servlet/src/main/kotlin/net/yan100/compose/depend/servlet/IReadableAttachmentFns.kt
@@ -1,15 +1,15 @@
 package net.yan100.compose.depend.servlet
 
 import net.yan100.compose.core.domain.IReadableAttachment
-import net.yan100.compose.core.typing.MimeTypes
 import org.springframework.web.multipart.MultipartFile
 
 fun MultipartFile.toReadableAttachment(): IReadableAttachment {
-  return IReadableAttachment[
-    originalFilename ?: name,
-    contentType ?: MimeTypes.BINARY.name,
-    isEmpty,
-    size,
-    bytes,
-    inputStream]
+  return IReadableAttachment.DefaultReadableAttachment(
+    name = this.originalFilename ?: this.name,
+    mimeType = this.contentType,
+    size = this.size,
+    bytes = { this.bytes },
+    inputStream = this.inputStream,
+    empty = this.isEmpty
+  )
 }
