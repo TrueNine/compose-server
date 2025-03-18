06dd6be0eb50107cc95731eeecf8ad49877a31a8
赵日天 <truenine304520@gmail.com>
2025-03-18 18:21:06 +0800

    build(version-catalog): 添加版本稳定性和更新检查
    - 新增 isNonStable 函数，用于判断版本是否为非稳定版本
    - 引入 ben-manes 的 grad

--- version-catalog/build.gradle.kts
+++ version-catalog/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import kotlin.jvm.optionals.getOrNull
 
 plugins {
@@ -18,10 +19,24 @@ dependencies {
       if (d.module.group.contains("net.yan100.compose") == false) {
         compileOnly(d)
       }
-      if (d.module.group == "cn.enaium" && d.module.name == "immutable-dependency") {
+      if (d.module.group.contains("cn.enaium") && d.module.name.contains("immutable-dependency")) {
         return@also
       }
     }
+  }
+}
+
+fun isNonStable(version: String): Boolean {
+  val stableKeyword = listOf("release", "final", "ga", "-jre").any { version.lowercase().contains(it) }
+  val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+  val isStable = stableKeyword || regex.matches(version)
+  return isStable.not()
+}
+
+// https://github.com/ben-manes/gradle-versions-plugin
+tasks.withType<DependencyUpdatesTask> {
+  this.rejectVersionIf {
+    isNonStable(candidate.version)
   }
 }
 
