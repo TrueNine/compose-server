65cb184e9f8ac7a2c99bebf59b5011c45fed7958
赵日天 <truenine304520@gmail.com>
2025-03-29 06:32:16 +0800

    build(version-catalog): 添加依赖版本控制和更新任务
    - 引入 ben-manes-versions 插件用于管理和更新依赖版本
    - 添加 compileOnly 依赖于非 "net.yan100.compose" 组的库
    - 排除特定的 "immutable-dependency" 库
    - 实现 isNonStable 函数检查非稳定版本
    - 配置 DependencyUpdatesTask 以拒绝非稳定版本的依赖更新

--- version-catalog/build.gradle.kts
+++ version-catalog/build.gradle.kts
@@ -1,11 +1,41 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import kotlin.jvm.optionals.getOrNull
+
 plugins {
   java
+  alias(libs.plugins.com.github.ben.manes.versions)
   `repositories-convention`
   `version-catalog`
   `publish-convention`
 }
 
 version = libs.versions.compose.asProvider().get()
+
+dependencies {
+  val allVersionCatalogs = rootProject.extensions.getByType<VersionCatalogsExtension>().named("libs")
+  allVersionCatalogs.libraryAliases.forEach { aliasLib ->
+    val dependency = allVersionCatalogs.findLibrary(aliasLib).getOrNull()?.get()
+    dependency?.also { d ->
+      if (d.module.group.contains("net.yan100.compose") == false) {
+        compileOnly(d)
+      }
+      if (d.module.group.contains("cn.enaium") && d.module.name.contains("immutable-dependency")) {
+        return@also
+      }
+    }
+  }
+}
+
+fun isNonStable(version: String): Boolean {
+  val nonStableKeywords = listOf("alpha", "beta", "rc", "cr", "m", "eap", "dev", "snapshot")
+  return nonStableKeywords.any { version.contains(it, true) }
+}
+// https://github.com/ben-manes/gradle-versions-plugin
+tasks.withType<DependencyUpdatesTask> {
+  rejectVersionIf {
+    isNonStable(candidate.version)
+  }
+}
 
 // https://github.com/ben-manes/gradle-versions-plugin
 catalog { versionCatalog { from(files("../gradle/libs.versions.toml")) } }
