3c53a47d2a87cce5230a521f3a016c750d8742ba
赵日天 <truenine304520@gmail.com>
2025-03-29 05:00:54 +0800

    build: 更新版本目录构建配置
    - 移除了 Kotlin 和 ben-manes 版本插件的引用
    - 删除了多余的依赖处理代码
    - 简化了版本目录的配置

--- version-catalog/build.gradle.kts
+++ version-catalog/build.gradle.kts
@@ -1,30 +1,11 @@
-import kotlin.jvm.optionals.getOrNull
-
 plugins {
   java
-  alias(libs.plugins.com.github.ben.manes.versions)
   `repositories-convention`
   `version-catalog`
   `publish-convention`
 }
 
 version = libs.versions.compose.asProvider().get()
-
-dependencies {
-  val allVersionCatalogs = rootProject.extensions.getByType<VersionCatalogsExtension>().named("libs")
-  allVersionCatalogs.libraryAliases.forEach { aliasLib ->
-    val dependency = allVersionCatalogs.findLibrary(aliasLib).getOrNull()?.get()
-    dependency?.also { d ->
-      if (d.module.group.contains("net.yan100.compose") == false) {
-        compileOnly(d)
-      }
-      if (d.module.group.contains("cn.enaium") && d.module.name.contains("immutable-dependency")) {
-        return@also
-      }
-    }
-  }
-}
-
 
 // https://github.com/ben-manes/gradle-versions-plugin
 catalog { versionCatalog { from(files("../gradle/libs.versions.toml")) } }
