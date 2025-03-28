3d3f39f4450db13778b537b2d4d6ecc1d1b9b1d3
赵日天 <truenine304520@gmail.com>
2025-03-29 06:32:30 +0800

    build: 更新版本目录并添加版本选择器
    - 在 build.gradle.kts 中添加版本选择器配置
    - 设置忽略特定版本（snapshot、alpha、beta）
    - 保持未使用的版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import nl.littlerobots.vcu.plugin.versionSelector
+
 plugins {
   // https://github.com/littlerobots/version-catalog-update-plugin
   alias(libs.plugins.nl.littlerobots.version.catalog.update)
@@ -12,6 +14,17 @@ idea {
 }
 
 versionCatalogUpdate {
+  versionSelector {
+    val g = it.candidate.group
+    val v = it.candidate.version
+    when {
+      g == libs.versions.group.get() -> false
+      v.lowercase().contains("snapshot") -> false
+      v.lowercase().contains("alpha") -> false
+      v.lowercase().contains("beta") -> false
+      else -> true
+    }
+  }
   keep {
     keepUnusedVersions = true
   }
