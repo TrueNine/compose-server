ec3f7ec1ea852680f930c167ef50b3491aa53cf2
赵日天 <truenine304520@gmail.com>
2025-04-10 18:18:57 +0800

    refactor(cacheable): 重构缓存相关配置和测试
    - 调整代码结构，优化 imports
    - 修复部分代码格式问题
    - 统一测试注解导入

--- cacheable/src/test/kotlin/net/yan100/compose/cacheable/autoconfig/AutoConfigurationBeanTest.kt
+++ cacheable/src/test/kotlin/net/yan100/compose/cacheable/autoconfig/AutoConfigurationBeanTest.kt
@@ -1,19 +1,18 @@
 package net.yan100.compose.cacheable.autoconfig
 
 import jakarta.annotation.Resource
+import kotlin.test.Test
+import kotlin.test.assertNotNull
 import net.yan100.compose.cacheable.CacheableEntrance
 import net.yan100.compose.slf4j
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cache.caffeine.CaffeineCacheManager
-import kotlin.test.Test
-import kotlin.test.assertNotNull
 
 private val log = slf4j<AutoConfigurationBeanTest>()
 
 @SpringBootTest(classes = [CacheableEntrance::class])
 class AutoConfigurationBeanTest {
-  @Resource
-  lateinit var caffeineCacheManager: CaffeineCacheManager
+  @Resource lateinit var caffeineCacheManager: CaffeineCacheManager
 
   @Test
   fun `has caffeine cache manager`() {
