cca2d0c7d4d0421ec5fef60be94909d6fb962a6a
赵日天 <truenine304520@gmail.com>
2025-04-18 10:07:05 +0800

    refactor(alias): 将 ID 类型相关扩展函数标记为 inline
    - 将 String 和 Number 类型的 isId、toId 函数修改为 inline，以提升性能
    - 保持函数逻辑不变，确保代码一致性

--- shared/src/main/kotlin/net/yan100/compose/AliasFns.kt
+++ shared/src/main/kotlin/net/yan100/compose/AliasFns.kt
@@ -8,17 +8,17 @@ inline fun Long.isId(): Boolean {
 
 /** ## 判断当前 Id 类型是否不为空 */
 @OptIn(ExperimentalContracts::class)
-fun String.isId(): Boolean {
+inline fun String.isId(): Boolean {
   return this != "" && this.all { it.isDigit() }
 }
 
 @Deprecated("框架内部调用代码，不应由用户直接调用", level = DeprecationLevel.ERROR)
 inline fun getDefaultNullableId(): Id = Long.MIN_VALUE
 
-fun Number.toId(): Id? {
+inline fun Number.toId(): Id? {
   return this.toLong().takeIf { it != Long.MIN_VALUE }
 }
 
-fun String.toId(): Id? {
+inline fun String.toId(): Id? {
   return this.toLongOrNull()?.takeIf { it != Long.MIN_VALUE }
 }
