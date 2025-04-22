a0b7fea1a097fd423a61b84965f1e97e0307c184
赵日天 <truenine304520@gmail.com>
2025-04-22 14:53:49 +0800

    build: 添加 Foojay 工具链插件支持
    - 在 settings.gradle.kts 中添加 Foojay 工具链插件，版本为 0.10.0
    - 启用类型安全项目访问特性预览

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 fun Pair<String, List<String>>.useFile() {
