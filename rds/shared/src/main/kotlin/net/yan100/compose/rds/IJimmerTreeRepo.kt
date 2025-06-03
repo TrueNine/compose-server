69d4757b3d3dde3eb42bf15b089058a438afe76d
赵日天 <truenine304520@gmail.com>
2025-06-03 20:13:27 +0800

    🗑 [rds-shared] 删除已废弃的 jpa 代码

--- rds/shared/src/main/kotlin/net/yan100/compose/rds/IJimmerTreeRepo.kt
+++
@@ -1,8 +0,0 @@
-package net.yan100.compose.rds
-
-import net.yan100.compose.rds.entities.IJimmerTreeEntity
-import org.springframework.data.repository.NoRepositoryBean
-
-@Deprecated("树结构已废弃")
-@NoRepositoryBean
-interface IJimmerTreeRepo<T : IJimmerTreeEntity, ID : Any> : IJimmerRepo<T, ID>
--- rds/shared/src/main/kotlin/net/yan100/compose/rds/entities/IExtensionDefineScope.kt
+++
@@ -1,4 +0,0 @@
-package net.yan100.compose.rds.entities
-
-/** # 数据库字段定义函数 */
-sealed interface IExtensionDefineScope
