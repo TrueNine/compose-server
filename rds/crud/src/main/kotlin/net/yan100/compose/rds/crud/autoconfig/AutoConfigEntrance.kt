86c19577dca46461ea59a248d5b50217c6698375
赵日天 <truenine304520@gmail.com>
2025-06-03 20:26:20 +0800

    🎉 [shared] 移除 jpa 的扫描配置

--- rds/crud/src/main/kotlin/net/yan100/compose/rds/crud/autoconfig/AutoConfigEntrance.kt
+++ rds/crud/src/main/kotlin/net/yan100/compose/rds/crud/autoconfig/AutoConfigEntrance.kt
@@ -3,22 +3,19 @@ package net.yan100.compose.rds.crud.autoconfig
 import org.babyfish.jimmer.spring.repository.EnableJimmerRepositories
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @EntityScan(
   basePackages =
-    ["net.yan100.compose.rds.crud.entities", "net.yan100.compose.rds.entities"]
-)
-@EnableJpaRepositories(
-  basePackages = ["net.yan100.compose.rds.crud.repositories.jpa"]
+    [
+      "net.yan100.compose.rds.crud.entities",
+    ]
 )
 @EnableJimmerRepositories(
-  basePackages = ["net.yan100.compose.rds.crud.repositories.jimmer"]
+  basePackages = ["net.yan100.compose.rds.crud.repositories"]
 )
 @ComponentScan(
   "net.yan100.compose.rds.crud.autoconfig",
   "net.yan100.compose.rds.crud.converters",
   "net.yan100.compose.rds.crud.service",
-  "net.yan100.compose.rds.crud.listener",
 )
 class AutoConfigEntrance
