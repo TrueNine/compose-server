f3826c9143e3e722dc5cac23d04eef2cfb9cb560
赵日天 <truenine304520@gmail.com>
2025-06-03 20:25:25 +0800

    🔥 [migration-h2] 移除 h2 支持

--- rds/migration-h2/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-  `java-convention`
-  `sqlmigration-convention`
-}
-
-version = libs.versions.compose.rds.get()
--- rds/migration-h2/src/main/resources/db/migration/V6__alter_user_info_add_remark_columns.sql
+++
@@ -1,5 +0,0 @@
-alter table
-    if exists user_info add column if not exists remark text default null;
-
-alter table
-    if exists user_info add column if not exists remark_name varchar(127) default null;
