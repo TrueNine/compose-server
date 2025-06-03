3ff084b9fddc4423f45664b04cf1e7f3793565f6
赵日天 <truenine304520@gmail.com>
2025-06-03 20:27:08 +0800

    🔨 [shared] 移除 jpa 以及 querydsl 祥鹏的依赖

--- rds/crud/build.gradle.kts
+++ rds/crud/build.gradle.kts
@@ -5,36 +5,24 @@ plugins {
 
 version = libs.versions.compose.rds.get()
 
-kapt {
-  javacOptions { option("querydsl.entityAccessors", "true") }
-  arguments { arg("plugin", "com.querydsl.apt.jpa.JPAAnnotationProcessor") }
-}
-
-ksp { arg("net.yan100.compose.ksp.plugin.generateJpa", "true") }
-
 dependencies {
   implementation(libs.org.jetbrains.kotlinx.kotlinx.coroutines.core)
-  api(libs.org.springframework.boot.spring.boot.starter.data.jpa)
-  implementation(libs.org.hibernate.orm.hibernate.core)
 
   implementation(libs.org.springframework.security.spring.security.crypto)
 
-  kapt(libs.com.querydsl.querydsl.apt)
-  implementation(libs.com.querydsl.querydsl.jpa)
-
-  ksp(projects.ksp.kspPlugin)
   ksp(libs.org.babyfish.jimmer.jimmer.ksp)
 
   implementation(libs.org.babyfish.jimmer.jimmer.spring.boot.starter)
 
   api(projects.shared)
-  implementation(projects.meta)
   api(projects.rds.rdsShared)
+  implementation(projects.meta)
 
   implementation(libs.com.fasterxml.jackson.core.jackson.databind)
 
   testImplementation(projects.security.securityCrypto)
   testImplementation(projects.testtoolkit)
   testImplementation(libs.org.flywaydb.flyway.core)
-  testImplementation(projects.rds.rdsMigrationH2)
+  testImplementation(libs.org.flywaydb.flyway.database.postgresql)
+  testImplementation(libs.org.postgresql.postgresql)
 }
