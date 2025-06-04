54ea651a2089b6b505f8609d2fa090c2df08a731
赵日天 <truenine304520@gmail.com>
2025-06-04 18:21:07 +0800

    💩 [deprecated] sql 迁移

--- rds/migration-postgres/src/main/resources/db/migration/V29__drop__other_tables.sql
+++ rds/migration-postgres/src/main/resources/db/migration/V29__drop__other_tables.sql
@@ -1,3 +1,25 @@
 drop
     table
-        if exists dis_cert_2;
+        if exists biz_cert;
+
+drop
+    table
+        if exists idcard_2;
+
+drop
+    table
+        if exists household_cert;
+
+drop
+    table
+        if exists bank_card;
+
+select
+    rm_presort_tree_struct('user_dept');
+
+alter table
+    if exists user_dept drop
+        column if exists rpi;
+
+select
+    rm_presort_tree_struct('address');
