f90ddd54ebb44be20538288437fdd2c240bcb523
赵日天 <truenine304520@gmail.com>
2025-06-05 21:48:24 +0800

    📷 [minio] 增加日志记录
    - 🐛 将连接错误提前到 MinioClient Bean 注册阶段

--- oss/minio/src/main/kotlin/net/yan100/compose/oss/minio/autoconfig/MinioAutoConfiguration.kt
+++ oss/minio/src/main/kotlin/net/yan100/compose/oss/minio/autoconfig/MinioAutoConfiguration.kt
@@ -21,7 +21,7 @@ import org.springframework.context.annotation.Configuration
 class MinioAutoConfiguration {
   companion object {
     @JvmStatic
-    private val log = slf4j(MinioAutoConfiguration::class)
+    private val log = slf4j<MinioAutoConfiguration>()
   }
 
   @Bean
@@ -32,11 +32,21 @@ class MinioAutoConfiguration {
   ): MinioClient {
     val accessUrl = ossProperties.baseUrl ?: minioProperties.endpoint
     val accessPort = ossProperties.port ?: minioProperties.endpointPort
-    log.trace("注册 minio accessUrl: {}", accessUrl)
-    return MinioClient.builder()
-      .endpoint(accessUrl, accessPort, minioProperties.enableHttps)
-      .credentials(minioProperties.accessKey, minioProperties.secretKey)
-      .build()
+    log.trace("register minio client accessUrl: {} , accessPort: {}", accessUrl, accessPort)
+
+    val minioClient =
+      MinioClient.builder().endpoint(accessUrl, accessPort, minioProperties.enableHttps).credentials(minioProperties.accessKey, minioProperties.secretKey)
+        .build()
+
+    try {
+      minioClient.listBuckets()
+      log.trace("minio client connected")
+    } catch (e: Exception) {
+      log.error("minio client connect failed", e)
+      throw e
+    }
+
+    return minioClient
   }
 
   @Bean
@@ -47,27 +57,13 @@ class MinioAutoConfiguration {
     ossProperties: OssProperties,
     minioProperties: MinioProperties,
   ): Oss {
-    try {
-      // 尝试执行一个简单的操作来验证连接
-      minioClient.listBuckets()
-      log.info("Minio 服务连接成功")
-    } catch (e: Exception) {
-      log.error("Minio 服务连接失败", e)
-      throw RuntimeException("Minio 服务连接失败，应用启动终止", e)
-    }
-
-    val exposeBaseUrl =
-      ossProperties.exposeBaseUrl
-        ?: minioProperties.exposedBaseUrl
-        ?: ossProperties.baseUrl
-        ?: minioProperties.endpoint
+    val exposeBaseUrl = ossProperties.exposeBaseUrl ?: minioProperties.exposedBaseUrl ?: ossProperties.baseUrl ?: minioProperties.endpoint
     log.trace(
       "register minio client wrapper, client: {} , exposeBaseUrl: {}",
       minioClient,
       exposeBaseUrl,
     )
     val clientWrapper = MinioClientWrapper(minioClient, exposeBaseUrl)
-
     return clientWrapper
   }
 }
