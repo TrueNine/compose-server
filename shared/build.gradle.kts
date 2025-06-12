158bcb6f61ccb4bcf89ffd7ef81995c987c7f637
赵日天 <truenine304520@gmail.com>
2025-06-13 05:27:30 +0800

    🎉 [inject] 添加 inject api 支持

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
   api(libs.com.fasterxml.jackson.core.jackson.annotations)
   api(libs.jakarta.annotation.jakarta.annotation.api)
   api(libs.jakarta.servlet.jakarta.servlet.api)
+  api(libs.jakarta.inject.jakarta.inject.api)
   api(libs.io.swagger.core.v3.swagger.annotations.jakarta)
   api(libs.org.slf4j.slf4j.api)
   implementation(projects.meta)
