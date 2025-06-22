efc5f2b345e6a2cb8ac3f6b66ec653a9d6b35a5f
赵日天 <truenine304520@gmail.com>
2025-06-22 17:03:56 +0800

    📝 [doc] 优化 ACID 注解文档与目标范围
    - ✨ 改进注释内容，明确 ACID 原则及事务回滚行为
    - ♻️ 调整 `@Target` 定义，新增对 `CLASS` 和 `TYPE` 的支持

--- rds/shared/src/main/kotlin/net/yan100/compose/rds/annotations/ACID.kt
+++ rds/shared/src/main/kotlin/net/yan100/compose/rds/annotations/ACID.kt
@@ -4,22 +4,20 @@ import org.springframework.transaction.annotation.Transactional
 import java.lang.annotation.Inherited
 
 /**
- * ## spring 中 事务注解 别称
- *
- * 对齐 默认加上了 Exception Throwable Error
- *
- * @see [Transactional]
- * @see [Exception]
- * @see [Throwable]
- * @see [Error]
- * @author TrueNine
- * @since 2024-09-11
+ * Marks a function, class, or property as adhering to the ACID (Atomicity, Consistency, Isolation, Durability) principles.
+ * This annotation ensures that the annotated element is transactional and will roll back in case of exceptions,
+ * including [Exception], [Throwable], and [Error], ensuring data integrity and reliability.
+ * The annotation is inheritable and retained at runtime, making it available for reflection.
+ * It can be applied to functions, classes, types, property getters, and property setters.
  */
 @Inherited
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 @Target(
+
   AnnotationTarget.FUNCTION,
+  AnnotationTarget.CLASS,
+  AnnotationTarget.TYPE,
   AnnotationTarget.PROPERTY_GETTER,
   AnnotationTarget.PROPERTY_SETTER,
 )
