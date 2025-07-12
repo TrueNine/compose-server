838ab7e1d586e33e4733b1b520b058ef30b31dc0
赵日天 <truenine304520@gmail.com>
2025-07-12 18:02:40 +0800

    ♻️ [buildlogic] 简化插件定义格式

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,9 +7,7 @@ pluginManagement {
   }
 }
 
-plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
