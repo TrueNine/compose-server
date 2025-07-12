799f3280ac32b821523633e72269ec60e408da4f
赵日天 <truenine304520@gmail.com>
2025-07-13 07:37:19 +0800

    ✨ [testtoolkit] update type aliases

--- testtoolkit/src/main/kotlin/io/github/truenine/composeserver/testtoolkit/Alias.kt
+++ testtoolkit/src/main/kotlin/io/github/truenine/composeserver/testtoolkit/Alias.kt
@@ -3,8 +3,6 @@ package io.github.truenine.composeserver.testtoolkit
 import org.junit.jupiter.api.io.TempDir
 import org.springframework.test.annotation.Rollback
 
-typealias SysLogger = org.slf4j.Logger
-
 typealias RDBRollback = Rollback
 
 /** 临时目录映射 */
