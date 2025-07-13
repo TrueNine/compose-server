91a74180134d630b595b8387c5327342e675cd15
赵日天 <truenine304520@gmail.com>
2025-07-14 07:20:38 +0800

    ♻️ [depend] 适配新 typing 接口系统
    - 🔄 Jackson 序列化适配
      - AnyTypingSerializer 适配 IAnyTyping
      - AnyTypingDeserializer 适配 IAnyTyping
      - JacksonAutoConfiguration 更新导入
    - 🔄 Servlet 转换器适配
      - AnyTypingConverterFactory 适配 IAnyTyping
    - 🔄 HTTP Exchange 适配
      - WebClientFns 适配 IAnyTyping
      - AnyTypingEncoder 适配 IAnyTyping

--- depend/http-exchange/src/main/kotlin/io/github/truenine/composeserver/depend/httpexchange/encoder/AnyTypingEncoder.kt
+++ depend/http-exchange/src/main/kotlin/io/github/truenine/composeserver/depend/httpexchange/encoder/AnyTypingEncoder.kt
@@ -1,6 +1,6 @@
 package io.github.truenine.composeserver.depend.httpexchange.encoder
 
-import io.github.truenine.composeserver.typing.AnyTyping
+import io.github.truenine.composeserver.IAnyTyping
 import org.reactivestreams.Publisher
 import org.springframework.core.ResolvableType
 import org.springframework.core.codec.AbstractEncoder
@@ -9,9 +9,9 @@ import org.springframework.core.io.buffer.DataBufferFactory
 import org.springframework.util.MimeType
 import reactor.core.publisher.Flux
 
-class AnyTypingEncoder : AbstractEncoder<AnyTyping>() {
+class AnyTypingEncoder : AbstractEncoder<IAnyTyping>() {
   override fun encode(
-    inputStream: Publisher<out AnyTyping>,
+    inputStream: Publisher<out IAnyTyping>,
     bufferFactory: DataBufferFactory,
     elementType: ResolvableType,
     mimeType: MimeType?,
