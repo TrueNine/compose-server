ebf902fc0774a0575eabafca5786689245fdc1d1
赵日天 <truenine304520@gmail.com>
2025-07-14 07:21:04 +0800

    ♻️ [security] 适配新 typing 接口系统
    - 🔄 OAuth2 模块适配
      - IWxpaWebClient 适配 IAnyTyping
      - WechatMpGrantTyping 适配 IStringTyping
    - 🔄 Spring Security 模块适配
      - JwtIssuer 适配 IAnyTyping
      - JwtVerifier 适配 IAnyTyping
      - SecurityExceptionAdware 适配 IAnyTyping

--- security/oauth2/src/main/kotlin/io/github/truenine/composeserver/security/oauth2/typing/WechatMpGrantTyping.kt
+++ security/oauth2/src/main/kotlin/io/github/truenine/composeserver/security/oauth2/typing/WechatMpGrantTyping.kt
@@ -1,7 +1,7 @@
 package io.github.truenine.composeserver.security.oauth2.typing
 
 import com.fasterxml.jackson.annotation.JsonValue
-import io.github.truenine.composeserver.typing.StringTyping
+import io.github.truenine.composeserver.IStringTyping
 
 /**
  * # 微信支付验证类型
@@ -9,7 +9,7 @@ import io.github.truenine.composeserver.typing.StringTyping
  * @author TrueNine
  * @since 2023-05-31
  */
-enum class WechatMpGrantTyping(private val typingCode: String) : StringTyping {
+enum class WechatMpGrantTyping(private val typingCode: String) : IStringTyping {
   CLIENT_CREDENTIAL("client_credential"),
   AUTH_CODE("authorization_code");
 
