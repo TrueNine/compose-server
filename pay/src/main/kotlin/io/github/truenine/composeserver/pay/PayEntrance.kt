d00e411162acd8f8aa7ee4ef08f2bf5ae96e0d68
赵日天 <truenine304520@gmail.com>
2025-07-19 13:33:29 +0800

    🧪 [test] 为项目添加更多单元测试

--- pay/src/main/kotlin/io/github/truenine/composeserver/pay/PayEntrance.kt
+++
@@ -1,7 +0,0 @@
-package io.github.truenine.composeserver.pay
-
-import io.github.truenine.composeserver.pay.autoconfig.AutoConfigEntrance
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Import
-
-@Import(AutoConfigEntrance::class) @SpringBootApplication internal class PayEntrance
--- pay/src/test/kotlin/io/github/truenine/composeserver/security/oauth2/schedule/wx/LoopWechatPublicAccountTest.kt
+++
@@ -1,5 +0,0 @@
-package io.github.truenine.composeserver.security.oauth2.schedule.wx
-
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest class LoopWechatPublicAccountTest
