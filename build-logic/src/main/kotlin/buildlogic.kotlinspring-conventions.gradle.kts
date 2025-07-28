e07f42593ac3f69349962ce915b1bd215a39f188
赵日天 <truenine304520@gmail.com>
2025-07-29 00:24:01 +0800

    [testtoolkit] 添加MySQL支持的构建配置
    -  添加mysql testcontainers依赖到版本目录
    -  更新testtoolkit构建脚本支持MySQL
    -  优化Kotlin Spring约定插件使用kapt

--- build-logic/src/main/kotlin/buildlogic.kotlinspring-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.kotlinspring-conventions.gradle.kts
@@ -1,12 +1,13 @@
 val libs = the<org.gradle.accessors.dm.LibrariesForLibs>()
 plugins {
-  id("buildlogic.javaspring-conventions")
+  id("buildlogic.javaspring-conventions") 
   id("buildlogic.kotlin-conventions")
   kotlin("plugin.spring")
+  kotlin("kapt")
 }
 
 dependencies {
-  annotationProcessor(libs.org.springframework.boot.spring.boot.configuration.processor)
+  kapt(libs.org.springframework.boot.spring.boot.configuration.processor)
 }
 
 // 配置 jar 任务包含 LICENSE 文件
