c24771eddfcbe2a7fc87484582293020ec45bd12
赵日天 <truenine304520@gmail.com>
2025-08-04 20:14:29 +0800

    refactor(depend): 重构 AnyTypingRecord 相关测试
    - 将 AnyTypingDeserializerTest重命名为 AnyEnumDeserializerTest
    - 将 AnyTypingRecord 重命名为 AnyEnumRecord
    - 更新测试用例和相关代码，以适应新的类名和包结构

--- depend/jackson/src/test/kotlin/io/github/truenine/composeserver/depend/jackson/AnyEnumDeserializerTest.kt
+++ depend/jackson/src/test/kotlin/io/github/truenine/composeserver/depend/jackson/AnyEnumDeserializerTest.kt
@@ -1,9 +1,9 @@
 package io.github.truenine.composeserver.depend.jackson
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.truenine.composeserver.enums.HttpStatus
+import io.github.truenine.composeserver.enums.UserAgents
 import io.github.truenine.composeserver.testtoolkit.log
-import io.github.truenine.composeserver.typing.HttpStatus
-import io.github.truenine.composeserver.typing.UserAgents
 import jakarta.annotation.Resource
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class AnyTypingDeserializerTest {
+class AnyEnumDeserializerTest {
   lateinit var mapper: ObjectMapper
     @Resource set
 
@@ -19,12 +19,12 @@ class AnyTypingDeserializerTest {
   fun `确保 撤销了枚举类型后，测试逻辑仍然可以运行`() {
     val stringTyping = UserAgents.CHROME_WIN_103
     val intTyping = HttpStatus._403
-    val dd = AnyTypingRecord(stringTyping, intTyping)
+    val dd = AnyEnumRecord(stringTyping, intTyping)
     val json = mapper.writeValueAsString(dd)
     log.info("json: {}", json)
     // Test that the JSON contains the actual values, not the enum names
     assertEquals("""{"stringTyping1":"${UserAgents.CHROME_WIN_103.value}","intTyping2":${HttpStatus._403.value}}""", json)
-    val des = mapper.readValue(json, AnyTypingRecord::class.java)
+    val des = mapper.readValue(json, AnyEnumRecord::class.java)
     log.info("des: {}", des)
     assertNotNull(des)
   }
--- depend/jackson/src/test/kotlin/io/github/truenine/composeserver/depend/jackson/AnyTypingRecord.kt
+++
@@ -1,6 +0,0 @@
-package io.github.truenine.composeserver.depend.jackson
-
-import io.github.truenine.composeserver.typing.HttpStatus
-import io.github.truenine.composeserver.typing.UserAgents
-
-data class AnyTypingRecord(var stringTyping1: UserAgents? = null, var intTyping2: HttpStatus? = null)
