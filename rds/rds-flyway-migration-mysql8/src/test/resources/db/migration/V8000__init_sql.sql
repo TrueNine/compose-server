8da0c039428d80f1bc4bb6c29ebc3c891d6e538f
赵日天 <truenine304520@gmail.com>
2025-08-08 07:35:33 +0800

    🎨 [style] Reformat `V8000__init_sql.sql` for consistent SQL indentation and alignment

--- rds/rds-flyway-migration-mysql8/src/test/resources/db/migration/V8000__init_sql.sql
+++ rds/rds-flyway-migration-mysql8/src/test/resources/db/migration/V8000__init_sql.sql
@@ -1,23 +1,13 @@
 create
-    table
-    if not exists test_user_account_table
-(
-    id
-    bigint
-    primary
-    key
-    AUTO_INCREMENT,
-    nick_name
-    varchar
-(
-    255
-) default null,
-    password_enc varchar
-(
-    1023
-) default null,
-    email varchar
-(
-    255
-) default null
-    );
+  table
+  if not exists test_user_account_table (
+                                          id
+                                                       bigint
+                                            primary
+                                              key
+                                            auto_increment,
+                                          nick_name
+                                                       varchar(255)  default null,
+                                          password_enc varchar(1023) default null,
+                                          email        varchar(255)  default null
+                                        );
