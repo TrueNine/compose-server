6ff7563dfb6dcd16972006a865e80acfe2744486
赵日天 <truenine304520@gmail.com>
2025-08-08 19:15:21 +0800

    🔥 [cleanup] Remove `Wxpa`-related configurations and imports
    - Deleted `WxpaAutoConfiguration` and `IWxpaWebClient` references from the project.
    - Simplified component scanning and configuration logic by eliminating unused `Wxpa` modules.

--- security/security-oauth2/src/main/kotlin/io/github/truenine/composeserver/security/oauth2/SecurityOauth2Entrance.kt
+++ security/security-oauth2/src/main/kotlin/io/github/truenine/composeserver/security/oauth2/SecurityOauth2Entrance.kt
@@ -1,7 +1,5 @@
 package io.github.truenine.composeserver.security.oauth2
 
-import io.github.truenine.composeserver.security.oauth2.autoconfig.WxpaAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Import
 
-@SpringBootApplication @Import(WxpaAutoConfiguration::class) class SecurityOauth2Entrance
+@SpringBootApplication class SecurityOauth2Entrance
