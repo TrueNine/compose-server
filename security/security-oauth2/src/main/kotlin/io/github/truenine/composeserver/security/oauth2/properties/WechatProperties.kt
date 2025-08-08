8aa63b6af334296d3feae75ebf2ac4a6595f93f3
赵日天 <truenine304520@gmail.com>
2025-08-08 19:15:06 +0800

    🔥 [cleanup] Remove deprecated `Wxpa`-related modules
    - Deleted all `Wxpa` classes, including `IWxpaWebClient`, `WxpaAutoConfiguration`, `WxpaProperty`, and associated models, properties, services, and schedules.
    - Simplified project structure by removing unused `Wxpa` logic and configurations.

--- security/security-oauth2/src/main/kotlin/io/github/truenine/composeserver/security/oauth2/properties/WechatProperties.kt
+++
@@ -1,8 +0,0 @@
-package io.github.truenine.composeserver.security.oauth2.properties
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.NestedConfigurationProperty
-
-private const val PREFIX = "compose.security.oauth2.wechat"
-
-@ConfigurationProperties(prefix = PREFIX) data class WechatProperties(@NestedConfigurationProperty var wxpa: WxpaProperties = WxpaProperties())
