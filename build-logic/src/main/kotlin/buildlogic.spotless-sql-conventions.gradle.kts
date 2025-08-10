2d3e2e640a3bce930a2c6254a45e5aeab53ad6e1
赵日天 <truenine304520@gmail.com>
2025-08-10 09:30:59 +0800

    🛠️ [chore] Remove redundant IDEA code style settings from Spotless SQL conventions

--- build-logic/src/main/kotlin/buildlogic.spotless-sql-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.spotless-sql-conventions.gradle.kts
@@ -9,14 +9,5 @@ spotless {
   sql {
     lineEndings = LineEnding.UNIX
     target("**/**.sql")
-    idea()
-      .codeStyleSettingsPath(
-        rootProject
-          .layout
-          .projectDirectory
-          .file(".idea/codeStyles/Project.xml")
-          .asFile
-          .absolutePath
-      )
   }
 }
