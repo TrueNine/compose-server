42d709f46a53f3e6f5e0db64eb6d4e3b985ab2cf
赵日天 <truenine304520@gmail.com>
2025-08-10 09:43:21 +0800

    🛠️ [chore] Replace hardcoded Minio property prefixes with centralized constants
    - Updated Minio Testcontainers configuration to use `SpringBootConfigurationPropertiesPrefixes`.
    - Enhanced maintainability by removing redundant hardcoded prefixes.
    - Adjusted corresponding test cases to align with the new constants.

--- testtoolkit/src/main/kotlin/io/github/truenine/composeserver/testtoolkit/SpringBootConfigurationPropertiesPrefixes.kt
+++ testtoolkit/src/main/kotlin/io/github/truenine/composeserver/testtoolkit/SpringBootConfigurationPropertiesPrefixes.kt
@@ -4,4 +4,11 @@ object SpringBootConfigurationPropertiesPrefixes {
   const val ROOT = "compose"
   const val TESTTOOLKIT = "$ROOT.testtoolkit"
   const val TESTTOOLKIT_TESTCONTAINERS = "$TESTTOOLKIT.testcontainers"
+  const val OSS = "$ROOT.oss"
+  const val OSS_MINIO = "$OSS.minio"
+  const val OSS_MINIO_ENDPOINT = "$OSS_MINIO.endpoint"
+  const val OSS_MINIO_PORT = "$OSS_MINIO.port"
+  const val OSS_MINIO_ACCESS_KEY = "$OSS_MINIO.access-key"
+  const val OSS_MINIO_SECRET_KEY = "$OSS_MINIO.secret-key"
+  const val OSS_MINIO_EXPOSED_BASE_URL = "$OSS_MINIO.exposed-base-url"
 }
