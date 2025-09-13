f3965c82380b03a2b560fdcfeb6c3cd956d0eeb8
赵日天 <truenine304520@gmail.com>
2025-09-13 08:13:28 +0800

    🔧 [config] Exclude `build-logic` module from auto-modules and enable typesafe project accessors

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,10 @@ plugins {
   id("io.gitee.zjarlin.auto-modules") version "0.0.608"
 }
 
+autoModules {
+  excludeModules("build-logic")
+}
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "compose-server"
